fb99d416bb10e24375bb89d7e43dedd73e968b66
James Ng <100321579+jng34@users.noreply.github.com>
2022-09-14 16:41:12 -0400

    Time: 105 ms (21.27%), Space: 42.3 MB (31.66%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if (!root) return [];
+    
+    let stack = [root];
+    let result = [];
+    
+    while (stack.length > 0) {
+        const node = stack.pop();
+        result.push(node.val);
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+    
+    return result;
+};
