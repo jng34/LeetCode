ed1b6ea2fc7e817495d77d80b724b0cb52729b56
James Ng <100321579+jng34@users.noreply.github.com>
2022-09-26 21:52:42 -0400

    Time: 91 ms (54.42%), Space: 41.6 MB (90.84%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.js
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.js
@@ -9,23 +9,41 @@
  * @param {ListNode} head
  * @return {number}
  */
+
 var getDecimalValue = function(head) {
     // Initialized result
     let sum = 0;
-    let length = 0;
-    // Set a pointer
-    let curr = head;
-    // Traverse linked list for length
-    while (curr != null) {
-        length++;
-        curr = curr.next;
-    }
 
-    // Traverse linked list for sum
+    // Traverse linked list
     while (head != null) {
-        sum = sum + (head.val * 2**(length - 1));
-        length--;
+        // Multiply result by 2 and add
+        sum = (sum * 2) + head.val
+
+        // Move next
         head = head.next;
     }
     return sum;
 }
+    
+
+
+// var getDecimalValue = function(head) {
+//     // Initialized result
+//     let sum = 0;
+//     let length = 0;
+//     // Set a pointer
+//     let curr = head;
+//     // Traverse linked list for length
+//     while (curr != null) {
+//         length++;
+//         curr = curr.next;
+//     }
+
+//     // Traverse linked list for sum
+//     while (head != null) {
+//         sum = sum + (head.val * 2**(length - 1));
+//         length--;
+//         head = head.next;
+//     }
+//     return sum;
+// }
