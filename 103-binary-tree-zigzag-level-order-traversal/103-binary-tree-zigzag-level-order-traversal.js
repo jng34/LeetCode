9ce523424f8c94c34875cfda02f1299a2cc98f33
James Ng <100321579+jng34@users.noreply.github.com>
2022-09-29 18:03:11 -0400

    Time: 101 ms (46.62%), Space: 44 MB (57.70%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
@@ -17,32 +17,31 @@
 var zigzagLevelOrder = function(root) {
     if (root === null) return [];
         // stack1 for all odd levels
-    let stack1 = [root];
-        // stack2 for all even levels
-    let stack2 = [];
-    let level = [];
-    let ans = [];
+    let stack1 = [root], stack2 = [], level = [], ans = [], reversal = true;
     
-    while (stack1.length !== 0 || stack2.length !== 0) {
-        while (stack1.length !== 0) {
-            let curr = stack1.pop();
-            level.push(curr.val);
-            if (curr.left !== null) stack2.push(curr.left);
-            if (curr.right !== null) stack2.push(curr.right);
+    while (stack1.length != 0) {
+        
+        let curr = stack1.pop();
+        level.push(curr.val);
+        
+        if (reversal) {
+            // set up stack for traversing right to left level
+            if (curr.left) stack2.push(curr.left);
+            if (curr.right) stack2.push(curr.right);
+        } else {
+            // set up stack for traversing left to right level
+            if (curr.right) stack2.push(curr.right);
+            if (curr.left) stack2.push(curr.left);
         }
-        ans.push(level);
-        level = [];
-        while (stack2.length !== 0) {
-            let curr = stack2.pop();
-            level.push(curr.val);
-            if (curr.right !== null) stack1.push(curr.right);
-            if (curr.left !== null) stack1.push(curr.left);
-        }
-        if (level.length !== 0) {
+        
+        if (stack1.length == 0) {
             ans.push(level);
             level = [];
-        }    
+            reversal = !reversal;
+            stack1 = stack2;
+            stack2 = [];
+        }
+        
     }
-    
     return ans;
 };
