39571e9c008f5dbafc8553e466536528bed70b14
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-13 15:36:41 -0400

    Time: 93 ms (59.59%), Space: 41.6 MB (97.55%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    /*
+            
+        
+        
+    */
+    return nums.indexOf(target) >= 0 ? nums.indexOf(target) : -1
+};
