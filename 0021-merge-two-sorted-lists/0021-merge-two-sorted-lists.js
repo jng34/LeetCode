29f53dafbbc671997dd410796a070df91a2a98c1
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-15 08:20:46 -0400

    Time: 116 ms (41.44%), Space: 44.5 MB (16.40%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,35 +11,26 @@
  */
 
 
-
-var mergeTwoLists = function(list1, list2) {
+// Recursive solution
+var mergeTwoLists = function (list1, list2) {
     
-    let head = new ListNode(0);
-    let tail = head;
+    // Base cases
+    if (!list1) return list2;
+    if (!list2) return list1;
     
-    while(tail) {
-        
-        if (!list1) {
-            tail.next = list2;
-            break;
-        }
-        if (!list2) {
-            tail.next = list1;
-            break;
-        }
-        
-        if (list1.val <= list2.val) {
-            tail.next = list1;
-            list1 = list1.next;
-        } else {
-            tail.next = list2;
-            list2 = list2.next;
-        }
-        
-        tail = tail.next;
+    // If list1's value is less than or equal to list2's value
+    // make recursive call to function on list1.next
+    // which will compare list1.next and current list2 values
+    // This will happen recursively until the base cases ^ are met
+    // Result:   1 -> 1 -> 2 -> 3 -> 4 -> 5
+    
+    if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
-    
-    return head.next;    
     
 }
 
@@ -62,12 +53,57 @@ var mergeTwoLists = function(list1, list2) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// Iterative solution
 // var mergeTwoLists = function(list1, list2) {
-//     //edge cases if either list1 or list2 are null
+    
+//     let head = new ListNode(0);
+//     let tail = head;
+    
+//     while(tail) {
+        
+//         if (!list1) {
+//             tail.next = list2;
+//             break;
+//         }
+//         if (!list2) {
+//             tail.next = list1;
+//             break;
+//         }
+        
+//         if (list1.val <= list2.val) {
+//             tail.next = list1;
+//             list1 = list1.next;
+//         } else {
+//             tail.next = list2;
+//             list2 = list2.next;
+//         }
+        
+//         tail = tail.next;
+//     }
+    
+//     return head.next;    
+    
+// }
+
+
+//Recursive solution
+// var mergeTwoLists = function(list1, list2) {
+    
 //     if (!list1) return list2;
 //     if (!list2) return list1;
-     
-//     if(list2.val >= list1.val) {
+    
+//     if (list1.val <= list2.val) {
 //         list1.next = mergeTwoLists(list1.next, list2);
 //         return list1;
 //     } else {
@@ -75,5 +111,16 @@ var mergeTwoLists = function(list1, list2) {
 //         return list2;
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+
 
 
