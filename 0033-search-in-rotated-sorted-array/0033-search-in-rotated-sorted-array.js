539b1110b08c9ff31461358cddb82491dd4862b2
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-17 09:43:19 -0400

    Time: 110 ms (23.48%), Space: 42.1 MB (53.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,6 +4,33 @@
  * @return {number}
  */
 var search = function(nums, target) {
-
-    return nums.indexOf(target) >= 0 ? nums.indexOf(target) : -1
+    
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        
+        let mid = Math.floor((left + right)/2);
+        if (nums[mid] === target) return mid;
+        
+        if (nums[left] <= nums[mid]) {
+            
+            if (target < nums[left] || target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            
+        } else {
+            
+            if (target > nums[right] || target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+    }
+    
+    return -1;
 };
