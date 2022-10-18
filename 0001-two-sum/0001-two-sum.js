4b1f907011de01c29493bc65d3649eba84ab3b1e
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-18 12:09:02 -0400

    Time: 94 ms (84.40%), Space: 42.8 MB (46.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i=0; i<nums.length-1; i++) {
-        for (let j=i+1; j<nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i,j];
-            }
-        }
+    let numsObj = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        
+        let diff = target - nums[i];
+        
+        if (diff in numsObj) return [numsObj[diff], i];
+        
+        numsObj[nums[i]] = i;
     }
-    return [];
+    return null;
 };
 
 // O(n^2) time complexity
