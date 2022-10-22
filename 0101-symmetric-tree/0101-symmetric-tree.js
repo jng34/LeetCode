f0f7e5a83de96cfb960ba0ee9a2232e26c7fc531
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-21 21:50:38 -0400

    Time: 128 ms (20.05%), Space: 44.5 MB (55.08%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -10,13 +10,38 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
+//Iterative
 var isSymmetric = function(root) {
     
-    function mirrorCheck(n1, n2) { 
-        if (!n1 && !n2) return true;
-        if (!n1 || !n2 || n1.val !== n2.val) return false;
-        return mirrorCheck(n1.left, n2.right) && mirrorCheck(n1.right, n2.left)
-    }
+    let queue = [root.left, root.right];
     
-    return mirrorCheck(root.left, root.right);
+    while(queue.length > 0) {
+        let node1 = queue.shift();
+        let node2 = queue.shift();
+        
+        if (!node1 & !node2) continue;
+        if (!node1 || !node2) return false;
+        if (node1.val !== node2.val) return false;
+        
+        queue.push(node1.left);
+        queue.push(node2.right);
+        queue.push(node1.right);
+        queue.push(node2.left);
+    }
+
+    return true;
+    
 }
+
+// Recursive
+// var isSymmetric = function(root) {
+    
+//     function mirrorCheck(n1, n2) { 
+//         if (!n1 && !n2) return true;
+//         if (!n1 || !n2 || n1.val !== n2.val) return false;
+//         return mirrorCheck(n1.left, n2.right) && mirrorCheck(n1.right, n2.left)
+//     }
+    
+//     return mirrorCheck(root.left, root.right);
+// }
