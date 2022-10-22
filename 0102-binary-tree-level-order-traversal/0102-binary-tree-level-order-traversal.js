ee071b352d1641bfc8417d04ca70edb95c5b5ca3
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-21 22:29:43 -0400

    Time: 102 ms (59.17%), Space: 44.4 MB (43.15%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+//Level order traversal aka breadth first traversal
+
+var levelOrder = function(root) {
+    
+    if (!root) return [];
+    let queue = [ root ];
+    let values = [];
+    
+    while(queue.length > 0) {
+        let qLength = queue.length;
+        let level = [];
+        for (let i = 0; i < qLength; i++) {
+            let curr = queue.shift();
+            if (curr.left) queue.push(curr.left);
+            if (curr.right) queue.push(curr.right);
+            level.push(curr.val);
+        }
+        values.push(level);
+    }
+    
+    return values;
+}
+
+// root = [3,9,20,8,5,15,7]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var levelOrder = function(root) {
+//     if (!root) return [];
+//     let queue = [root];
+//     let level = [];
+  
+//     while (queue.length > 0) {
+//         //determine length of queue
+//         let miniQueue = queue.length;
+//         let miniLevel = [];
+//         //loop through to include only nodes at each level 
+//         for (let i = 0; i < miniQueue; i++) {
+//             const curr = queue.shift();
+//             if (curr.left) queue.push(curr.left);
+//             if (curr.right) queue.push(curr.right);
+//             miniLevel.push(curr.val)
+//         }
+//         //push arr of each level to output array
+//         level.push(miniLevel);
+//     }
+//     return level;
+// };
