603a67a7e8aaefa59fd9a579af486c99f042bcdf
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-21 20:27:04 -0400

    Time: 97 ms (46.32%), Space: 42.5 MB (35.83%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -11,9 +11,34 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+
+//Iterative Solution
 var isSameTree = function(p, q) {
-    if (!p && !q) return true;
-    if (!p || !q) return false;
-    if (p.val !== q.val) return false;
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    let queue = [p, q];
+    while (queue.length > 0) {
+        let pNode = queue.shift();
+        let qNode = queue.shift();
+        
+        if (!pNode && !qNode) continue;
+        if (!pNode || !qNode || pNode.val !== qNode.val) return false;
+        
+        queue.push(pNode.left);
+        queue.push(qNode.left);
+        queue.push(pNode.right);
+        queue.push(qNode.right);
+        
+    }
+    
+    return true;
 }
+
+
+
+
+// Recursive Solution
+// var isSameTree = function(p, q) {
+//     if (!p && !q) return true;
+//     if (!p || !q) return false;
+//     if (p.val !== q.val) return false;
+//     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+// }
