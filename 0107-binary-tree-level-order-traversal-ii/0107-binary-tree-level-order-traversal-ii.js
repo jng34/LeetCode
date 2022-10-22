04eeeca85a2fde1b34c749b3422ae8d79b239155
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-22 09:16:38 -0400

    Time: 92 ms (73.68%), Space: 44.3 MB (40.13%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.js
@@ -10,21 +10,47 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+
+//Iterative solution
 var levelOrderBottom = function(root) {
+    if (!root) return [];
     
+    let queue = [root];
     let values = [];
-    let level = 0;
-    traverseLevel(root, level);
-    return values.reverse();
     
-    function traverseLevel(node, level) {
-        if(!node) return;
-        if(level >= values.length) {
-            values[level] = [];
+    while(queue.length > 0) {
+        const qLen = queue.length;
+        let level = [];
+        for (let i = 0; i < qLen; i++) {
+            let curr = queue.shift();
+            level.push(curr.val);
+            
+            if (curr.left) queue.push(curr.left);
+            if (curr.right) queue.push(curr.right);
         }
-        values[level].push(node.val);
-        level++;
-        traverseLevel(node.left, level);
-        traverseLevel(node.right, level);
-    } 
-};
+        values.push(level);
+    }
+    
+    
+    return values.reverse();
+}
+
+//Recursive Solution
+// var levelOrderBottom = function(root) {
+    
+//     let values = [];
+//     let level = 0;
+//     traverseLevel(root, level);
+//     return values.reverse();
+    
+//     function traverseLevel(node, level) {
+//         if(!node) return;
+//         if(level >= values.length) {
+//             values[level] = [];
+//         }
+//         values[level].push(node.val);
+//         level++;
+//         traverseLevel(node.left, level);
+//         traverseLevel(node.right, level);
+//     } 
+// };
