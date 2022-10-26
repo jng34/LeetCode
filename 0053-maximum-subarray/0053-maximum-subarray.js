dbcf861259606099a6aa2eeb546e34b96162df86
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-26 16:29:43 -0400

    Time: 82 ms (97.04%), Space: 49.5 MB (99.10%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,14 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// O(n) time and space
 var maxSubArray = function(nums) {
     
-    let memo = { 0: nums[0] };
+    let currMax = nums[0];
     let max = nums[0];
     
     for (let i = 1; i < nums.length; i++) {
-        memo[i] = Math.max(memo[i-1] + nums[i], nums[i]);
-        max = Math.max(max, memo[i]);
+        currMax = Math.max(currMax + nums[i], nums[i]);
+        max = Math.max(max, currMax);
     }
     
     return max;
@@ -17,6 +19,22 @@ var maxSubArray = function(nums) {
 
 
 
+//O(n) time and space
+// var maxSubArray = function(nums) {
+    
+//     let memo = { 0: nums[0] };
+//     let max = nums[0];
+    
+//     for (let i = 1; i < nums.length; i++) {
+//         memo[i] = Math.max(memo[i-1] + nums[i], nums[i]);
+//         max = Math.max(max, memo[i]);
+//     }
+    
+//     return max;
+// };
+
+
+// O(n^2) time, O(1) space - NO GOOD
 // var maxSubArray = function(nums) {
 //     // Max call stack exceeded
 //     if (nums.length == 1) return nums[0];
