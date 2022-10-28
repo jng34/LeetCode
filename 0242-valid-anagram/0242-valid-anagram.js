a9bd88f76d2baf219a291a99d1151daed87d0021
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-28 12:29:47 -0400

    Time: 148 ms (39.10%), Space: 45.9 MB (38.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,22 +3,37 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isAnagram = function(s, t) {
     
     if (s.length !== t.length) return false;
     
-    let lettersObj = {};
-    for (let char of s) {
-        lettersObj[char] = (lettersObj[char] || 0) + 1;
-    }
-    
-    for (let char of t) {
-        if (!lettersObj[char]) {
-            return false;
-        } else {
-            lettersObj[char] -= 1;
-        }
+    let s1 = [...s].sort();
+    let t1 = [...t].sort();
+
+    for (let i = 0; i < s.length; i++) {
+        if (s1[i] !== t1[i]) return false;
     }
     
     return true;
 };
+
+// var isAnagram = function(s, t) {
+    
+//     if (s.length !== t.length) return false;
+    
+//     let lettersObj = {};
+//     for (let char of s) {
+//         lettersObj[char] = (lettersObj[char] || 0) + 1;
+//     }
+    
+//     for (let char of t) {
+//         if (!lettersObj[char]) {
+//             return false;
+//         } else {
+//             lettersObj[char] -= 1;
+//         }
+//     }
+    
+//     return true;
+// };
