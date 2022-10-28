4878a2cb3f287c5860d68d5e04c09af865f63ea4
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-28 14:25:23 -0400

    Time: 111 ms (95.16%), Space: 45.2 MB (92.44%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -2,13 +2,42 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var maximumProduct = function(nums) {
+
+    let min1 = Infinity, min2 = Infinity, max1 = -Infinity, max2 = -Infinity, max3 = -Infinity;
     
-    let sortNums = nums.sort((a,b) => a-b);
-    let len = nums.length;
+    for (let n of nums) {
+        if (n <= min1) {
+            min2 = min1;
+            min1 = n;
+        } else if (n <= min2) {
+            min2 = n;
+        }
+        
+        if (n >= max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = n;
+        } else if (n >= max2) {
+            max3 = max2;
+            max2 = n;
+        } else if (n >= max3) {
+            max3 = n;
+        }
+    };
     
-    return Math.max(
-        sortNums[len-1]*sortNums[len-2]*sortNums[len-3], 
-        sortNums[0]*sortNums[1]*sortNums[len-1]
-    );
+    return Math.max(min1*min2*max1,  max1*max2*max3);
 };
+
+
+// var maximumProduct = function(nums) {
+
+//     let sortNums = nums.sort((a,b) => a-b);
+//     let len = nums.length;
+    
+//     return Math.max(
+//         sortNums[len-1]*sortNums[len-2]*sortNums[len-3], 
+//         sortNums[0]*sortNums[1]*sortNums[len-1]
+//     );
+// };
