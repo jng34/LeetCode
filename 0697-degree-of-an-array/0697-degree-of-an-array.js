f9ada839755eaa38f05d5e1129cef98a8ee70ab2
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-29 15:02:11 -0400

    Time: 144 ms (57.82%), Space: 45.6 MB (87.41%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.js
+++ 0697-degree-of-an-array/0697-degree-of-an-array.js
@@ -4,32 +4,55 @@
  */
 var findShortestSubArray = function(nums) {
     // Use an object to store frequency of element occurrences 
-    let freq = {};
-    for (let n of nums) {
-        freq[n] = (freq[n] || 0) + 1;
+    let left = {}, right = {}, freq = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (!(nums[i] in left)) left[nums[i]] = i;
+        right[nums[i]] = i;
+        freq[nums[i]] = (freq[nums[i]] || 0) + 1;
     };
     
-    // let start, end, s = 1, count = 0;
-    let maxFreq = Math.max(...Object.values(freq));
-    let keys = Object.keys(freq).filter(key => freq[key] === maxFreq)
-    let smallest = Infinity;
+    let smallest = nums.length;
+    let degree = Math.max(...Object.values(freq));
     
-    // For each element that has same degree
-    for (let i = 0; i < keys.length; i++) {
-        // Get start and end indices
-        let start = 0, end = nums.length-1, len;
-        while(start < nums.length) {
-            if (nums[start] === Number(keys[i])) break;
-            start++;
-        }
-        while(end >= 0) {
-            if (nums[end] === Number(keys[i])) break;
-            end--;
-        }
-        // console.log(start, end)
-        len = end - start + 1;
-        smallest = Math.min(smallest, len);
-    }
+    for (let n in freq) {
+        if (freq[n] === degree) {
+            smallest = Math.min(smallest, right[n] - left[n] + 1);
+        };
+    };
     
     return smallest;
+
 };
+
+
+// var findShortestSubArray = function(nums) {
+//     // Use an object to store frequency of element occurrences 
+//     let freq = {};
+//     for (let n of nums) {
+//         freq[n] = (freq[n] || 0) + 1;
+//     };
+    
+//     // let start, end, s = 1, count = 0;
+//     let maxFreq = Math.max(...Object.values(freq));
+//     let keys = Object.keys(freq).filter(key => freq[key] === maxFreq)
+//     let smallest = Infinity;
+    
+//     // For each element that has same degree
+//     for (let i = 0; i < keys.length; i++) {
+//         // Get start and end indices
+//         let start = 0, end = nums.length-1, len;
+//         while(start < nums.length) {
+//             if (nums[start] === Number(keys[i])) break;
+//             start++;
+//         }
+//         while(end >= 0) {
+//             if (nums[end] === Number(keys[i])) break;
+//             end--;
+//         }
+//         // console.log(start, end)
+//         len = end - start + 1;
+//         smallest = Math.min(smallest, len);
+//     }
+    
+//     return smallest;
+// };
