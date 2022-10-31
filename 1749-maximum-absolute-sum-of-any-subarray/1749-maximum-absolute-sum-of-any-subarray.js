eabc0cfcbc6e42ee34796d3d0f326b96f6db85f2
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-31 14:56:38 -0400

    Time: 93 ms (91.89%), Space: 47.9 MB (27.03%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.js
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.js
@@ -4,39 +4,50 @@
  */
 var maxAbsoluteSum = function(nums) {
     
-    if (nums.length === 1) return Math.abs(nums[0]);
-    
     let currSum = 0;
     let maxSum = 0;
     let minSum = 0;
     
-    for (let i = 0; i < nums.length; i++) {
-        currSum = currSum + nums[i];
-        // console.log("currSum: " + currSum)
-        if (maxSum < currSum) {
-            maxSum = currSum
-        }
-        if (currSum < 0) {
-            currSum = 0
-        }
-        // console.log("maxSum: " + maxSum)
-        // if (maxSum < 0) maxSum = 0
+    for (let num of nums) {
+        currSum += num;
+        maxSum = Math.max(maxSum, currSum);
+        minSum = Math.min(minSum, currSum);
     }
     
-    currSum = 0
-    
-    for (let i = 0; i < nums.length; i++) {
-        currSum = currSum + nums[i];
-        console.log("currSum: " + currSum)
-        if (minSum > currSum) {
-            minSum = currSum
-        }
-        if (currSum > 0) {
-            currSum = 0
-        }
-        console.log("minSum: " + minSum)
-    }
-    
-    return Math.max(maxSum, Math.abs(minSum))
-
+    return maxSum - minSum;
 };
+
+// var maxAbsoluteSum = function(nums) {
+    
+//     if (nums.length === 1) return Math.abs(nums[0]);
+    
+//     let currSum = 0;
+//     let maxSum = 0;
+//     let minSum = 0;
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         currSum = currSum + nums[i];
+//         if (maxSum < currSum) {
+//             maxSum = currSum
+//         }
+//         if (currSum < 0) {
+//             currSum = 0
+//         }
+
+//     }
+    
+//     currSum = 0
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         currSum = currSum + nums[i];
+//         if (minSum > currSum) {
+//             minSum = currSum
+//         }
+//         if (currSum > 0) {
+//             currSum = 0
+//         }
+//     }
+    
+//     return Math.max(maxSum, Math.abs(minSum))
+
+// };
