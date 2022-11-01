10b86e9fa88f360225511bd94bbd364270e64d03
James Ng <100321579+jng34@users.noreply.github.com>
2022-10-31 22:02:28 -0400

    Time: 97 ms (64.55%), Space: 43.7 MB (12.17%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -10,37 +10,59 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    
-    // Basically, if head is the only node, then return null
-    if (!head.next) return null;
-    
-    //Traverse through L.L. to determine length
-    let size = 0, curr = head;
-    while (curr) {
-        curr = curr.next;
-        size++;
-    }
 
-    // Find 0th-index of node to remove
-    // Break the link at the node 1 before index (index - 1)
-    let index = size - n, i = 0;
-    curr = head;
-    
-    if (index === 0) return head.next;
-    while (i < index) {
-        if (i === index - 1){
-            if (!curr.next.next) {
-                curr.next = null;  
-            } else {
-                curr.next = curr.next.next;
-            }
-            break;
-        }
-        
-        curr = curr.next;
-        i++;
-    }
-    
-    return head;
+
+// 0 -> 1 -> 2 -> 3 -> 4 -> 5
+//      B                   F
+var removeNthFromEnd = function(head, n) {
+  const root = new ListNode(0);
+  root.next = head;
+  let front = root;
+  let back = root;
+  while (n >= 0) {
+    front = front.next;
+    n--;
+  }
+  while (front) {
+    front = front.next;
+    back = back.next;
+  }
+  back.next = back.next.next;
+  return root.next;
 };
+
+// var removeNthFromEnd = function(head, n) {
+    
+//     // Basically, if head is the only node, then return null
+//     if (!head.next) return null;
+    
+//     //Traverse through L.L. to determine length
+//     let size = 0, curr = head;
+//     while (curr) {
+//         curr = curr.next;
+//         size++;
+//     }
+
+//     // Find 0th-index of node to remove
+//     // Break the link at the node 1 before index (index - 1)
+//     let index = size - n, i = 0;
+//     curr = head;
+    
+//     if (index === 0) return head.next;
+    
+//     while (i < index) {
+//         if (i === index - 1){
+//             if (!curr.next.next) {
+//                 curr.next = null;  
+//             } else {
+//                 curr.next = curr.next.next;
+//             }
+//             break;
+//         }
+        
+//         curr = curr.next;
+//         i++;
+//     }
+    
+//     return head;
+// };
