df38e56df3ab336a16689fe01bf6d306141bbcfc
James Ng <100321579+jng34@users.noreply.github.com>
2022-11-03 14:51:43 -0400

    Time: 120 ms (5.40%), Space: 41.9 MB (54.99%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+/*
+   initialize
+   ways = 0
+   use recursion
+*/
+
+var climbStairs = function(n) {
+    
+    // if n is 1, then return 1
+    // if n is 2, then return 2
+    // for n steps, we have to add the # of ways for the previous two steps
+    // fibonacci seq
+    // use tabulation 
+    let table = new Array(n+1).fill(0);
+    table[1] = 1;
+    table[2] = 2;
+    
+    for (let i = 3; i < table.length; i++) {
+        table[i] = table[i-1] + table[i-2];
+    }
+    return table[n];
+}
+
+
+
+
+
+
+// var climbStairs = function(n){
+//     let [one, two] = [1, 1];
+    
+//     for (let i = 0; i < n-1; i++) {
+//         let current = one;
+//         one = one + two;
+//         two = current;
+//     }
+//     return one;
+// };
+
+
+
+
