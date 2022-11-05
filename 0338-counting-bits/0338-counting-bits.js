72e3b928e49489681fffaf3275d0e5658df7b497
James Ng <100321579+jng34@users.noreply.github.com>
2022-11-04 20:44:33 -0400

    Time: 1676 ms (5.04%), Space: 60.8 MB (5.60%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -2,32 +2,64 @@
  * @param {number} n
  * @return {number[]}
  */
+
 var countBits = function(n) {
     /*
-     Brute Force Method:
-     - Iterate from 0 to n
-     - Convert each num to binary number using n.toString(2)
-     - Then count # of 1s in each num (string type)
-     - Push result in ans array
-     - Return ans array
-     
-     Time: O(n^2) -nested for loops
-     Space: O(n)
+        Optimized solution: 
+        -Use recursion/helper function
+        -Array of 1s for each n basically same as previous array of 1s, 
+        -but includes n's number of 1s
     */
     
     let ans = [];
-    
-    for (let i = 0; i <= n; i++) {
+
+    function countOnes(num, ans) {
+        if (num === 0) {
+            ans.unshift(0);
+            return ans;
+        }
         
-        let binary = i.toString(2);
+        let binary = num.toString(2);
         let count = 0;
+        
         for (let digit of binary) {
             if (digit === '1') count++;
         }
+        ans.unshift(count);
         
-        ans.push(count);
-        
-    } 
+        return countOnes(num-1, ans);
+    }
     
-    return ans;
+    return countOnes(n, ans);
 };
+
+
+// var countBits = function(n) {
+//     /*
+//      Brute Force Solution:
+//      - Iterate from 0 to n
+//      - Convert each num to binary number using n.toString(2)
+//      - Then count # of 1s in each num (string type)
+//      - Push result in ans array
+//      - Return ans array
+     
+//      Time: O(n^2) -nested for loops
+//      Space: O(n)
+//     */
+    
+//     let ans = [];
+    
+//     for (let i = 0; i <= n; i++) {
+        
+//         let binary = i.toString(2);
+//         let count = 0;
+//         for (let digit of binary) {
+//             if (digit === '1') count++;
+//         }
+        
+//         ans.push(count);
+        
+//     } 
+    
+//     return ans;
+// };
