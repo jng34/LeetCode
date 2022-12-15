8d0b8feda57b7e176462b03a9bfb666259d8ce7a
James Ng <100321579+jng34@users.noreply.github.com>
2022-12-15 15:24:34 -0500

    Time: 81 ms (87.69%), Space: 45.6 MB (15.13%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,25 +11,36 @@
  * @return {boolean}
  */
 
+var hasCycle = function(head) {
+  let seen = new Set();
+  let curr = head;
+  while (curr) {
+    if (seen.has(curr)) return true;
+    seen.add(curr);
+    curr = curr.next;
+  }
+  return false;
+};
+
 //  3  -  2  -  0  -  (-4)  - 2  -  0  -  (-4)
 //                    slow  
 //                                        fast
  
-var hasCycle = function(head) {
-  if (!head || !head.next) return false;
+// var hasCycle = function(head) {
+//   if (!head || !head.next) return false;
   
-  let slow = head, fast = head;
+//   let slow = head, fast = head;
   
-  while(fast) {
-    if (fast.next) {
-      slow = slow.next;
-      fast = fast.next.next;
-    } else {
-      return false;
-    }
+//   while(fast) {
+//     if (fast.next) {
+//       slow = slow.next;
+//       fast = fast.next.next;
+//     } else {
+//       return false;
+//     }
     
-    if (slow === fast) return true;
-  }
+//     if (slow === fast) return true;
+//   }
   
-  return false;
-};
+//   return false;
+// };
