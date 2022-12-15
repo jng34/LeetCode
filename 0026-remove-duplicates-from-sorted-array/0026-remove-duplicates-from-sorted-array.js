05a459d758d34bdea50a51572e59f3d0fa43e0b4
James Ng <100321579+jng34@users.noreply.github.com>
2022-12-15 13:54:17 -0500

    Time: 90 ms (85.18%), Space: 44.9 MB (38.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,15 +3,17 @@
  * @return {number}
  */
 
-
+//  1  1  2  2  2 
+//              i
+//        p
 var removeDuplicates = function(nums) {
-  let prev = 0, curr = 1;
-  while (curr < nums.length) {
-    if (nums[curr] == nums[prev]) {
-      nums.splice(curr, 1);
-    } else {
-      prev = curr
-      curr++;
+  let pointer = 1, i = 1;
+  while (i < nums.length) {
+    if (nums[i - 1] !== nums[i]) {
+      nums[pointer] = nums[i];
+      pointer++;
     }
+    i++;
   }
+  return pointer;
 };
