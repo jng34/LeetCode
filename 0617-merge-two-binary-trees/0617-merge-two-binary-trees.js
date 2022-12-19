367d999228ef01a5656b42e95eee81675642259c
James Ng <100321579+jng34@users.noreply.github.com>
2022-12-18 20:20:28 -0500

    Time: 107 ms (85.48%), Space: 49.9 MB (74.92%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -16,9 +16,9 @@ var mergeTrees = function(root1, root2) {
   if (!root1) return root2;
   if (!root2) return root1;
   
-  const left = mergeTrees(root1.left, root2.left);
-  const right = mergeTrees(root1.right, root2.right);
-  const newNode = new TreeNode(root1.val + root2.val, left, right);
+  let newNode = new TreeNode(root1.val + root2.val);
+  newNode.left = mergeTrees(root1.left, root2.left);
+  newNode.right = mergeTrees(root1.right, root2.right);
   
   return newNode;
 };
