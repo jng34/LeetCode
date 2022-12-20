6f9a26735f08b5ac51aa7b083ffd46a8f26f561e
James Ng <100321579+jng34@users.noreply.github.com>
2022-12-20 18:05:31 -0500

    Time: 51 ms (41.97%), Space: 13.9 MB (61.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -1,14 +1,14 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var rob = function(nums, i = 0, memo = {}) {
-  if (i in memo) return memo[i]
-  if (i >= nums.length) return 0;
-  
-  const include = nums[i] + rob(nums, i+2, memo);
-  const exclude = rob(nums, i+1, memo);    
-  memo[i] = Math.max(include, exclude)
-  
-  return memo[i];
-};
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+      
+      def _rob(nums, i, memo):  
+        if i in memo:
+          return memo[i]
+        if i >= len(nums):
+          return 0
+        include_first = nums[i] + _rob(nums, i+2, memo)
+        exclude_first = _rob(nums, i+1, memo)
+        memo[i] = max(include_first, exclude_first)
+        return memo[i]
+    
+      return _rob(nums, 0, {})
