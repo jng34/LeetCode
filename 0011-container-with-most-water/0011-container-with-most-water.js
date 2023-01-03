54d8d8288f96c582a4df0b4f2a49d6181c3d5df8
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-03 12:42:42 -0500

    Time: 79 ms (89.12%), Space: 49.9 MB (23.55%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -2,10 +2,16 @@
  * @param {number[]} height
  * @return {number}
  */
+
+/*
+    [5,8,6,2,5,4,7,5]
+     0 1 2 3 4 5 6 7   
+
+*/
 var maxArea = function(height) {
     let left = 0, right = height.length - 1, max = 0, currMax;
     while (left < right) {
-        if (height[left] <= height[right]) {
+        if (height[left] < height[right]) {
             currMax = (right - left) * height[left];
             left++;
         } else {
