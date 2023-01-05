33812f6abe92e92a7525b1221f3039f4d7ab5fd6
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-04 19:10:14 -0500

    Time: 97 ms (47.20%), Space: 42.9 MB (51.23%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -4,37 +4,21 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-    let s2 = "", t2 = "", i = 0;
-    
-    while (i < s.length) {
-        if (s[i] === '#') {
-            s2 = s2.slice(0,-1);
-            i++;
-            // continue;
-        } else if (s[i+1] === '#') {
-            i += 2;
-            // continue;
-        } else {
-            s2 += s[i];
-            i++;
-        }
-    }
-      
-    i = 0;
-    
-    while (i < t.length) {
-        if (t[i] === '#') {
-            t2 = t2.slice(0,-1);
-            i++;
-            // continue;
-        } else if (t[i+1] === '#') {
-            i += 2;
-            // continue;
-        } else {
-            t2 += t[i];
-            i++;
-        }
-    }
-    // console.log(s2, t2)
-    return s2 === t2;
+    return parseStr(s) === parseStr(t);
 };
+
+function parseStr(str) {
+    let i = 0, newStr = '';
+    while (i < str.length) {
+        if (str[i] === '#') {
+            newStr = newStr.slice(0,-1);
+            i++;
+        } else if (str[i+1] === '#') {
+            i += 2;
+        } else {
+            newStr += str[i];
+            i++;
+        }
+    }
+    return newStr;
+}
