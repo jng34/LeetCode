007990bb20826a44ef81ced74ac37b53c8cb3467
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-10 13:17:27 -0500

    Time: 64 ms (84.91%), Space: 15.2 MB (67.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,9 @@
-/**
- * @param {number[]} prices
- * @return {number}
- */
-
-
-/*
-    [7,1,5,3,6,4]
-     i
-       l
-     h
-*/
-var maxProfit = function(prices) {
-    let profit = 0, lowest = prices[0];
-    for (let i = 1; i < prices.length; i++) {
-        lowest = Math.min(lowest, prices[i]);
-        if (prices[i] - lowest > 0) {
-            profit += prices[i] - lowest;
-            lowest = prices[i];
-        }
-    }
-    return profit;
-};
-
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            prev = prices[i-1]
+            curr = prices[i]
+            if curr > prev:
+                profit += curr - prev
+        return profit
