d77e10ddcaad902dbf23581b3026e5af99ef1d20
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-10 18:35:49 -0500

    Time: 75 ms (74.74%), Space: 43.3 MB (95.47%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.js
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -6,18 +6,16 @@
 // [ 1, 1, 0 ]   i=0
 // [ 1, 0, 1 ]   i=1
 // [ 0, 0, 0 ]   i=2
+
+/*
+   [0,1,1]
+   [1,0,1]
+   [0,0,0]
+*/
 var flipAndInvertImage = function(image) {
-    let newImage = [];
     for (let i = 0; i < image.length; i++) {
-        let flipped = image[i].reverse()
-        for (let j = 0; j < flipped.length; j++) {
-            if (flipped[j] === 1) {
-                flipped[j] = 0;
-            } else {
-                flipped[j] = 1;
-            }
-        }
-        newImage.push(flipped);
+        image[i].reverse();
+        image[i] = image[i].map(el => el === 1 ? 0 : 1)
     }
-    return newImage;
+    return image;
 };
