247b0c6ed4785919f2ca8e904d84d91cd291d3b6
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-13 10:51:35 -0500

    Time: 91 ms (57.52%), Space: 44 MB (84.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,25 +4,25 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-  let allIntervals = [...intervals, newInterval];
-  allIntervals.sort((a,b) => a[0]-b[0]);
-  console.log(allIntervals)
-  let result = [];
-  let start = allIntervals[0][0];
-  let end = allIntervals[0][1];
-  
-  for (let i = 1; i < allIntervals.length; i++) {
-    const [currStart, currEnd] = allIntervals[i];
-    if (currStart <= end) {
-      end = Math.max(end, currEnd);
+  let [newIntStart, newIntEnd] = newInterval;
+  let leftIntervals = [], rightIntervals = [];
+  for (let interval of intervals) {
+    const [currStart, currEnd] = interval;
+    
+    // If current end is greater than newInterval start, add to left intervals
+    if (currEnd < newIntStart) {
+      leftIntervals.push(interval);
+      
+    // If newInterval end is less than current start, add to right intervals
+    } else if (newIntEnd < currStart) {
+      rightIntervals.push(interval);
+      
+    // If currEnd is greater than or equal to newInterval start, merge the two intervals
     } else {
-      result.push([start, end]);
-      start = currStart;
-      end = currEnd;
+      newIntStart = Math.min(newIntStart, currStart);
+      newIntEnd = Math.max(newIntEnd, currEnd);
     }
   }
   
-  result.push([start, end]);
-  
-  return result;
-};
+  return [ ...leftIntervals, [newIntStart, newIntEnd], ...rightIntervals ];
+}
