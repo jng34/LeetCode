e89694104b2cf88a18836c7dc8f49c6f1748f538
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-18 18:31:16 -0500

    Time: 464 ms (78.59%), Space: 44.8 MB (74.30%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -4,34 +4,19 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let left = 0, right = n - 1;
   
-  //Search for target in targeted arrays
-  var helper = function(startRow, endRow) {
-    let col = 0;
-    for (let row = startRow; row <= endRow; row++) {
-      for (let col = 0; col < matrix[row].length; col++) {
-        if (matrix[row][col] === target) return true;
-        if (matrix[row][col] > target) break;
-      }
-    }
-    return false;
-  }
-  
-  //Binary search for possible array containing target
-  let left = 0, right = matrix.length - 1;
-  while (left <= right) {
-    if (target < matrix[right][0]) {
-      right -= 1;
-    } else if (target > matrix[right][0]) {
-      if (target <= matrix[right][matrix[right].length - 1]) {
-        console.log(left, right)
-        return helper(left, right);
-      } else {
-        left += 1;
-      }
-    } else {
+  while (left < m && right >= 0) {
+    if (matrix[left][right] === target) {
       return true;
+    } else if (matrix[left][right] < target) {
+      left++;
+    } else {
+      right--;
     }
   }
+  
   return false;
 };
