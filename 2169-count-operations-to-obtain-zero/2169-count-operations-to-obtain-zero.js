435db277d5ca4f1c3f5ffa429b68182ae32c7390
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-17 21:53:44 -0500

    Time: 75 ms (57.04%), Space: 42.2 MB (38.73%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.js
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.js
@@ -3,10 +3,11 @@
  * @param {number} num2
  * @return {number}
  */
-var countOperations = function(num1, num2, count = 0) {
-  if (num1 === 0 || num2 === 0) return count;
-  
-  num1 >= num2 ? num1 -= num2 : num2 -= num1;
-  
-  return countOperations(num1, num2, count + 1);
+var countOperations = function(num1, num2) {
+  let count = 0;
+  while (num1 > 0 && num2 > 0) {
+    num1 >= num2 ? num1 -= num2 : num2 -= num1;
+    count++;
+  }
+  return count;
 };
