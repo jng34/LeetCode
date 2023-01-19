c22b84f57576d8a94811ec934513783fded53b57
James Ng <100321579+jng34@users.noreply.github.com>
2023-01-19 12:33:17 -0500

    Time: 84 ms (92.29%), Space: 50.1 MB (87.49%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+var subarraySum = function(nums, k) {
+  const freq = new Map();
+  freq.set(0,1);
+  let sum = 0, count = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    let diff = sum - k;
+    if (freq.has(diff)) {
+      count += freq.get(diff);
+    }
+    if (freq.has(sum)) {
+      freq.set(sum, freq.get(sum) + 1);
+    } else {
+      freq.set(sum, 1);
+    }
+  }
+  
+  return count;
+}
+
+// Brute force
+// var subarraySum = function(nums, k) {
+//   let count = 0, tempSum = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     tempSum = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       tempSum += nums[j];
+//       if (tempSum === k) count++;
+//     }
+//   } 
+//   return count;
+// };
