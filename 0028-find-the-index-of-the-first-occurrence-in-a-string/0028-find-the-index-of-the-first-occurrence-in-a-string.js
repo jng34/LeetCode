af5c97a40096154658e957655186bfe162b52f01
James Ng <100321579+jng34@users.noreply.github.com>
2023-02-06 17:41:08 -0500

    Time: 63 ms (74.17%), Space: 42.3 MB (36.21%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+  // Base case if haystack does not include need, return -1
+  if (!haystack.includes(needle)) return -1;
+  // Split haystack with needle
+  // e.g. haystack = 'dinosaur' and needle = 'saur'
+  // 'dinosaur'.split('saur') = ['dino', '']
+  // The first occurence of 'saur will be right after the first word 'dino'
+  return haystack.split(needle)[0].length;
 };
