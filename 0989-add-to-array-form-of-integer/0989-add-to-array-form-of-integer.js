3ef3c8922bbe04956d9aee6cbbd53918a7371dfa
James Ng <100321579+jng34@users.noreply.github.com>
2023-02-15 19:24:57 -0500

    Time: 101 ms (90.32%), Space: 46.3 MB (93.84%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function(num, k) {
+  num.reverse();
+  const n = String(k).split('').reverse();
+  
+  let remainder = 0;
+  let i = 0;
+  let sum;
+  
+  while (i < n.length || i < num.length) {
+    if (n[i] && num[i]) {
+      sum = Number(n[i]) + num[i] + remainder;
+    } else if (n[i]) {
+      sum = Number(n[i]) + remainder;
+    } else {
+      sum = num[i] + remainder;
+    }
+    
+    if (sum > 9) {
+      num[i] = sum % 10;
+      remainder = 1;
+    } else {
+      num[i] = sum;
+      remainder = 0;
+    }
+    i++;
+  }
+  
+  if (remainder === 1) num.push(1);
+  
+  return num.reverse();
+};
