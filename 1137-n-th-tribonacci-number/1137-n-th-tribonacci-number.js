b57a1742125067b1974decc6fc2f6355c2a41d73
James Ng <100321579+jng34@users.noreply.github.com>
2023-02-20 19:34:56 -0500

    Time: 77 ms (5.41%), Space: 41.7 MB (54.73%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -2,17 +2,27 @@
  * @param {number} n
  * @return {number}
  */
-var tribonacci = function(n) {
+var tribonacci = function(n, memo={}) {
+  // Top Down Approach - memoization
+  // base cases
+  if (n in memo) return memo[n];
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
+  
+  memo[n] = tribonacci(n-3, memo) + tribonacci(n-2, memo) + tribonacci(n-1, memo);
+  return memo[n];
+  
+  
   // Bottom Up Approach - tabulation method
-  const seq = new Array(n + 1).fill(0);
-  seq[0] = 0;
-  seq[1] = 1;
-  seq[2] = 1;
+//   const seq = new Array(n + 1).fill(0);
+//   seq[0] = 0;
+//   seq[1] = 1;
+//   seq[2] = 1;
   
-  for (let i = 3; i < seq.length; i++) {
-    seq[i] += seq[i-3] + seq[i-2] + seq[i-1];
-  }
+//   for (let i = 3; i < seq.length; i++) {
+//     seq[i] += seq[i-3] + seq[i-2] + seq[i-1];
+//   }
   
-  console.log(seq)
-  return seq[n];
+//   return seq[n];
+  
 };
