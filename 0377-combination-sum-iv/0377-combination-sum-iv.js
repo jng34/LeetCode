f2dc2e35f06d774f4116594d9ea5f9dc2e87b250
James Ng <100321579+jng34@users.noreply.github.com>
2023-02-20 19:05:21 -0500

    Time: 65 ms (78.23%), Space: 44.3 MB (13.27%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.js
+++ 0377-combination-sum-iv/0377-combination-sum-iv.js
@@ -3,21 +3,38 @@
  * @param {number} target
  * @return {number}
  */
-var combinationSum4 = function(nums, target, memo={}) {
-  // Base cases
-  if (target < 0) return 0; // no possible combos
-  if (target === 0) return 1; // only possible combo is []
-  if (target in memo) return memo[target];
+var combinationSum4 = function(nums, target) {
+  // Bottom Up - Tabulation method
+  nums.sort((a,b) => a-b);
+  const combos = new Array(target + 1).fill(0);
+  // 1 combo to make target of 0...empty array []
+  combos[0] = 1
   
-  let count = 0;
-  
-  for (let num of nums) {
-    if (num <= target) {
-      const diff = target - num;
-      count += combinationSum4(nums, diff, memo);
+  for (let t = 0; t < combos.length; t++) {
+    for (let num of nums) {
+      if (num > t) break;
+      combos[t] += combos[t - num];
     }
   }
   
-  memo[target] = count;
-  return count;
+  return combos[target];  
+  
+  // Top Down - Memoization method
+  // Base cases
+//   if (target < 0) return 0; // no possible combos
+//   if (target === 0) return 1; // only possible combo is []
+//   if (target in memo) return memo[target];
+  
+//   let count = 0;
+  
+//   for (let num of nums) {
+//     if (num <= target) {
+//       const diff = target - num;
+//       count += combinationSum4(nums, diff, memo);
+//     }
+//   }
+  
+//   memo[target] = count;
+//   return count;
+  
 };
