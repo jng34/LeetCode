64eed5452c97309ec7acd4cf462fb5d5ceb6cc2f
James Ng <100321579+jng34@users.noreply.github.com>
2023-02-22 10:30:01 -0500

    Time: 116 ms (57.92%), Space: 48.6 MB (36.99%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -3,33 +3,18 @@
  * @param {number} k
  * @return {number[][]}
  */
-
-/*
-  Iterate through range of nums [1, n] and store in array
-  Apply recursion to find all possible combos
-  Time: O(n!/[k!(n-k)!])
-*/
 var combine = function(n, k) {
-  let nums = [];
-  for (let i = 1; i <= n; i++) {
-    nums.push(i);
+  const solution = [];
+  function backTrack(k, path, next) {
+    if (k === 0) return solution.push(path.slice());
+    
+    for (let i = next; i < n + 1; i++) {
+      path.push(i);
+      backTrack(k - 1, path, i + 1)
+      path.pop();
+    }
   }
   
-  return findCombos(nums, k);
+  backTrack(k, [], 1);
+  return solution;
 };
-
-
-var findCombos = function(arr, num) {
-  if (arr.length < num) return [];
-  if (num === 0) return [[]];
-  
-  const first = arr[0];
-  const combos1 = findCombos(arr.slice(1), num - 1);
-  const allCombos = [];
-  for (let combo of combos1) {
-    allCombos.push([first, ...combo]);
-  }
-  
-  const combos2 = findCombos(arr.slice(1), num);
-  return [...allCombos, ...combos2];
-}
