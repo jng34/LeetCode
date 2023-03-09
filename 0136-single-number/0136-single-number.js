30fba40baff94c5699370bab417e3a3ac6046d85
James Ng <100321579+jng34@users.noreply.github.com>
2023-03-09 16:16:45 -0500

    Time: 83 ms (47.62%), Space: 47.3 MB (23.70%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -9,14 +9,9 @@ var singleNumber = function(nums) {
     freq[num] = (freq[num] || 0) + 1;
   }
   
-  //Find min count from array of values
-  const minFreq = Math.min(...Object.values(freq));
-  
   //Find the num associated with minFreq
   for (let key in freq) {
-    if (freq[key] === minFreq) {
-      return key;
-    }
+    if (freq[key] === 1) return key;
   }
   
 };
