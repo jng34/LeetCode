31b627e794d9f7175dd0795476571c5d43df2e41
James Ng <100321579+jng34@users.noreply.github.com>
2023-03-15 14:26:16 -0400

    Time: 53 ms (97.77%), Space: 42.7 MB (43.70%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -13,15 +13,11 @@ var isValid = function(s) {
     }
     for (let i = 0; i < s.length; i++) {
         const char = s[i];
-        if ( char === "(" || char === "{" || char === "[" ) {
-            stack.push(char);
+        const last = stack[stack.length-1];  
+        if (obj[last] === char) {
+          stack.pop();
         } else {
-          const last = stack[stack.length-1];  
-          if (obj[last] === char) {
-            stack.pop();
-          } else {
-            stack.push(char)
-          }
+          stack.push(char);
         }
     }
     return stack.length === 0;
