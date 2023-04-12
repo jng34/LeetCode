bb7e1f4d1906606169ca499c7cb81cafa731844a
James Ng <100321579+jng34@users.noreply.github.com>
2023-04-11 22:54:08 -0400

    Time: 54 ms (83.16%), Space: 41.7 MB (82.95%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,28 +3,14 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    let numsArr = [...String(num)];
-    let maxNum = num;
-    
-    for (let i=0; i<numsArr.length; i++) {
-        // For each index, change 9 to 6 or 6 to 9
-        if (numsArr[i] === '9') {
-            numsArr[i] = '6';
-        } else {
+    let numsArr = [...String(num)];    
+    let i = 0;
+    while (i < numsArr.length) {
+        if (numsArr[i] === '6') {
             numsArr[i] = '9';
-        }
-        
-        // Convert back to number and compare max 
-        const newNum = Number(numsArr.join(''));
-        maxNum = Math.max(maxNum, newNum);
-        
-        // Convert back to original number
-        if (numsArr[i] === '9') {
-            numsArr[i] = '6';
-        } else {
-            numsArr[i] = '9';
-        }   
+            break;
+        } 
+        i++;
     }
-
-    return maxNum;
+    return Number(numsArr.join(''));
 };
