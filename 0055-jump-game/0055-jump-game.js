13339e7a0674dc7eb1d84059a007c5c3c00db06e
James Ng <100321579+jng34@users.noreply.github.com>
2023-04-17 17:22:42 -0400

    Time: 81 ms (43.14%), Space: 47.2 MB (31.35%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -11,6 +11,10 @@ var canJump = function(nums) {
   const end = nums.length;
   
   for (let i=1; i<end; i++) {
+    // if previous jumps = 0, return false
+    if (jumps[i-1] === 0) return false;
+    
+    // Check if index is reachable by seeing if previous jumps > 0
     if (jumps[i-1] > 0) {
       jumps[i] = Math.max(jumps[i-1]-1, nums[i]);
     }
