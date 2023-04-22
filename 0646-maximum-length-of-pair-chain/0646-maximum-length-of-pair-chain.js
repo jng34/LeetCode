f6c0be890906fbb2d00479644956febb6c344fcf
James Ng <100321579+jng34@users.noreply.github.com>
2023-04-21 21:01:16 -0400

    Time: 127 ms (48.15%), Space: 51.3 MB (14.81%) - LeetHub

---
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} pairs
+ * @return {number}
+ */
+var findLongestChain = function(pairs) {
+  pairs.sort((p1, p2) => p1[1]-p2[1]);
+  console.log(pairs)
+  let longest = 1;
+  let b = pairs[0][1];
+  
+  for (let i=1; i<pairs.length; i++) {
+    const [c, d] = pairs[i]; 
+    if (b < c) {
+      longest += 1;
+      b = d;
+    }   
+  }
+  
+  return longest;
+};
