cea639bb1c8c8b031de8c6c3ce7cd5871279dec5
James Ng <100321579+jng34@users.noreply.github.com>
2023-04-25 15:31:18 -0400

    Time: 54 ms (86.36%), Space: 41.4 MB (94.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,22 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    // [2, 0, 2, 1, 0, 1]
-    //            j  i
-    //              idx
-    let index = 0;
-    let i, j;
-    
-    while (index < nums.length) {
-        i = nums.length-1;
-        j = nums.length-2;
-        while (j >= index) {
-            if (nums[i] < nums[j]) {
-                [ nums[i], nums[j] ] = [ nums[j], nums[i] ]
+    // [0, 2, 0, 2, 1, 1]
+    //  i
+    //  j
+
+    for (let i=0; i<nums.length; i++) {
+        for (let j=nums.length-1; j>=1; j--) {
+            if (nums[j] < nums[j-1]) {
+                [ nums[j], nums[j-1] ] = [ nums[j-1], nums[j] ]
             }
-            i--;
-            j--;
         }
-        index++;
     }
 };
