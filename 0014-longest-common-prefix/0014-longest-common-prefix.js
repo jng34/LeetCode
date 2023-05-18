3167586e9da641fd7f1281b316b0cd3ac6dc92f6
James Ng <100321579+jng34@users.noreply.github.com>
2023-05-18 12:40:43 -0400

    Time: 70 ms (24.73%), Space: 42.3 MB (60.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,15 +4,12 @@
  */
 var longestCommonPrefix = function(strs) {
     strs.sort((a,b) => a.length-b.length)
-    // console.log(strs)
     const shortest = strs[0];
     let prefix = '';
     for (let char of shortest) {
         prefix += char;
         for (let i=1; i<strs.length; i++) {
             //check if remaining words starts with prefix
-            console.log(strs[i])
-            console.log(prefix)
             if (!strs[i].startsWith(prefix)) {
                 prefix = prefix.slice(0, prefix.length-1);
                 return prefix;
