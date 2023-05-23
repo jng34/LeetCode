0aad9cefb67c2107706719e7ed676b47af749492
James Ng <100321579+jng34@users.noreply.github.com>
2023-05-23 17:34:51 -0400

    Time: 90 ms (20.91%), Space: 48.3 MB (16.09%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+  const freqMap = {};
+  const bucket = [];
+  const result = [];
+
+  for(let num of nums) {
+      freqMap[num] = (freqMap[num] || 0) + 1;
+  }
+
+  for(let [num, freq] of Object.entries(freqMap)) {
+      bucket[freq] = (bucket[freq] || new Set()).add(num);
+  }
+
+  for(let i = bucket.length-1; i >= 0; i--) {
+    if(bucket[i]) result.push(...bucket[i]);
+    if(result.length === k) break;
+  }
+  
+  return result;
+};
