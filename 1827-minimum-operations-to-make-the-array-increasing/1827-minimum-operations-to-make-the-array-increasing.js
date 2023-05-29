8cf3b5813464c86211bb0d8a31a05327d58e0d92
James Ng <100321579+jng34@users.noreply.github.com>
2023-05-29 15:50:15 -0400

    Time: 72 ms (31.49%), Space: 43.9 MB (78.45%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.js
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.js
@@ -5,9 +5,9 @@
 var minOperations = function(nums) {
   let ops = 0;
   for (let i=1; i<nums.length; i++) {
-    while (nums[i] <= nums[i-1]) {
-      nums[i]++;
-      ops++;
+    if (nums[i] <= nums[i-1]) {
+      ops += nums[i-1] - nums[i] + 1;
+      nums[i] = nums[i-1] + 1;
     }
   }
   return ops;
