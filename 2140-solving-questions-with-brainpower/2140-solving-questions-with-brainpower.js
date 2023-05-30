2f911c5fa9d73fbf8e5b35bc8a6d3739fd7a0fad
James Ng <100321579+jng34@users.noreply.github.com>
2023-05-30 14:17:20 -0400

    Time: 251 ms (20.96%), Space: 89 MB (33.41%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.js
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var mostPoints = function(questions) {
+    let memo={};
     
-    function answerQuestion(index, memo={}) {
+    function answerQuestion(index) {
         // Base cases
         if (index >= questions.length) return 0;
         if (index in memo) return memo[index];
@@ -13,10 +14,10 @@ var mostPoints = function(questions) {
         
         // Choose to solve the current question and 
         // skip the next 'brainpower' number of questions;
-        const solve = pts + answerQuestion(index + bp + 1, memo); 
+        const solve = pts + answerQuestion(index + bp + 1); 
         
         // Choose to skip and answer the next question;
-        const skip = answerQuestion(index + 1, memo);
+        const skip = answerQuestion(index + 1);
         
         let maxPoints = Math.max(skip, solve);
         
