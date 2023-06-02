16b74a935b92e176f8edb3314ac600db31a741dd
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-02 14:10:42 -0400

    Time: 242 ms (42.71%), Space: 49.6 MB (98.96%) - LeetHub

--- 0648-replace-words/0648-replace-words.js
+++ 0648-replace-words/0648-replace-words.js
@@ -6,11 +6,11 @@
 var replaceWords = function(dictionary, sentence) {
     let strArr = sentence.split(' ');
     for (let i=0; i<strArr.length; i++) {
-        for (let root of dictionary) {
+        dictionary.forEach((root) => {
             if (strArr[i].startsWith(root)) {
                 strArr[i] = root;
             }
-        }
+        })
     }
     return strArr.join(' ');
 };
