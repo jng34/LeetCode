d87cc3c298f0e6803f74f9b1fc9bded63fb92d77
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-05 12:39:34 -0400

    Time: 323 ms (26.00%), Space: 42.1 MB (53.00%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -3,25 +3,20 @@
  * @return {number}
  */
 var countVowelStrings = function(n) {
-    // Gather all permutations
-    const vowels = ["a", "e", "i", "o", "u"];
-    let allPerms = [];
-    vowels.forEach((vowel) => findAllPerms(0, n, vowel, allPerms, vowels));
-    return allPerms.length;
-};
+    
+    function count(n, index) {
+        if (n === 0) {
+            res++;
+            return;
+        }
 
-function findAllPerms(index, num, str, res, arr) {
-    if (str.length === num) {
-        res.push(str);
-        return;
-    }
-        
-    for (let i=index; i<arr.length; i++) {
-        if (arr[i] === str[str.length-1]) {
-            findAllPerms(index, num, str+arr[i], res, arr);
-        }
-        if (arr[i] > str[str.length-1]) {
-            findAllPerms(index+1, num, str+arr[i], res, arr);
+        for (let i=index; i<5; i++) {
+           count(n-1, i)
         }
     }
-}
+    
+    let res = 0;
+    count(n, 0);
+    
+    return res;
+};
