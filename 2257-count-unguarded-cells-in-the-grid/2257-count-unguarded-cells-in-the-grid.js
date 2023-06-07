3fd3ae9e59900d3a071f6c06284054efff7bd222
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-07 19:09:55 -0400

    Time: 422 ms (50.00%), Space: 100.3 MB (20.00%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.js
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.js
@@ -7,42 +7,49 @@
  */
 var countUnguarded = function(m, n, guards, walls) {
     const grid = Array(m).fill().map(() => Array(n).fill(''))
-    let guarded = new Set();
-
+    let unGuarded = 0;
     //Mark walls    
     walls.forEach((wall) => grid[wall[0]][wall[1]] = 'W');
     //Mark guards
     guards.forEach((guard) => grid[guard[0]][guard[1]] = 'G');
     
-    for (let [x,y] of guards) { 
+    for (let [x,y] of guards) {
+        
         //Check left of guard
         for (let i=y-1; i>=0; i--) {
             if (grid[x][i] ===  'W' || grid[x][i] ===  'G') break;
-            if (guarded.has(x+','+i)) continue;
-            guarded.add(x+','+i);
+            if (grid[x][i] ===  'x') continue;
+            grid[x][i] = 'x';
         }
         
         //Check right of guard
         for (let i=y+1; i<grid[x].length; i++) {
             if (grid[x][i] ===  'W' || grid[x][i] ===  'G') break;
-            if (guarded.has(x+','+i)) continue;
-            guarded.add(x+','+i);
+            if (grid[x][i] ===  'x') continue;
+            grid[x][i] = 'x';
         }
         //Check above of guard
         for (let i=x-1; i>=0; i--) {
             if (grid[i][y] ===  'W' || grid[i][y] ===  'G') break;
-            if (guarded.has(i+','+y)) continue;
-            guarded.add(i+','+y);
+            if (grid[i][y] ===  'x') continue;
+            grid[i][y] = 'x';
         }
         
         //Check below of guard
         for (let i=x+1; i<grid.length; i++) {
             if (grid[i][y] ===  'W' || grid[i][y] ===  'G') break;
-            if (guarded.has(i+','+y)) continue;
-            guarded.add(i+','+y);
+            if (grid[i][y] ===  'x') continue;
+            grid[i][y] = 'x';
         }
     }
     
-    const unGuarded = (m*n)-(guards.length)-(walls.length)-(guarded.size);
+    console.log(grid)
+    
+    for (let r=0; r<grid.length; r++) {
+        for (let c=0; c<grid[0].length; c++) {
+            if (grid[r][c] === '') unGuarded++;
+        }
+    }
+    
     return unGuarded;
 };
