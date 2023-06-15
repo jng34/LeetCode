e19a3f35d81f1c90628fb3384ace61e2d7640816
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-15 14:48:14 -0400

    Time: 153 ms (81.46%), Space: 77.4 MB (5.37%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -14,16 +14,16 @@ var maxLevelSum = function(root) {
     // Keep track of levels and maxSum 
     let sumAndLevel = {};
     // BFS
-    let queue = [[root, 1]];
-    while (queue.length > 0) {
-        const [node, level] = queue.shift();
+    let stack = [[root, 1]];
+    while (stack.length > 0) {
+        const [node, level] = stack.pop();
         if (sumAndLevel[level]) {
             sumAndLevel[level] += node.val;
         } else {
             sumAndLevel[level] = node.val;
         }
-        if (node.left) queue.push([ node.left, level+1 ]);
-        if (node.right) queue.push([ node.right, level+1 ]);
+        if (node.right) stack.push([ node.right, level+1 ]);
+        if (node.left) stack.push([ node.left, level+1 ]);
     }
     
     let maxSum = Math.max(...Object.values(sumAndLevel));
