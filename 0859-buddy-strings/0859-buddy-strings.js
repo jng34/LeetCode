034cfad6b43c378f7d7a7738a273232b13d24509
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-17 14:55:20 -0400

    Time: 78 ms (13.49%), Space: 45.6 MB (5.16%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+    if (s.length === 1) return false;
+    if (s.length !== goal.length) return false;
+    
+    let mapStr = {};
+    if (s === goal) {
+        for (let i=0; i<s.length; i++) {
+            mapStr[s[i]] = (mapStr[s[i]] || 0) + 1;
+        }
+    }
+    
+    if (Object.keys(mapStr).length > 1) {
+        for (let key in mapStr) {
+            if (mapStr[key] > 1) return true;
+        }
+        return false;   
+    }
+    
+    let strArr = s.split('');
+    let diffIndex = [];
+    for (let i=0; i<s.length; i++) {
+        if (s[i] !== goal[i]) diffIndex.push(i);
+    }
+    const first = strArr[diffIndex[0]];
+    strArr[diffIndex[0]] = strArr[diffIndex[1]];
+    strArr[diffIndex[1]] = first;
+    return strArr.join('') === goal;        
+};
