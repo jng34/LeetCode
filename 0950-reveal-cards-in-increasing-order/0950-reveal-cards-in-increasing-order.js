8d96d70d44ac3ee6d82495c9c2dc01fa6ec8cf0e
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-19 22:21:05 -0400

    Time: 68 ms (65.45%), Space: 43.8 MB (83.64%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.js
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.js
@@ -3,12 +3,12 @@
  * @return {number[]}
  */
 const deckRevealedIncreasing = deck => {
-    deck.sort((a, b) => a - b);
+    deck.sort((a, b) => b - a);
     const newDeck = [];
     while (deck.length > 0) {
-        newDeck.unshift(deck.pop());
-        newDeck.unshift(newDeck.pop());
+        newDeck.push(deck.shift());
+        newDeck.push(newDeck.shift());
     }
-    newDeck.push(newDeck.shift());
-    return newDeck;
+    newDeck.unshift(newDeck.pop());
+    return newDeck.reverse();
 };
