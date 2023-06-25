b0bf7414c77cab782b4ee4cc8fb9ca089e09409c
James Ng <100321579+jng34@users.noreply.github.com>
2023-06-24 22:37:00 -0400

    Time: 63 ms (48.28%), Space: 42.9 MB (33.33%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
@@ -5,25 +5,17 @@
  */
 var uncommonFromSentences = function(s1, s2) {
     let uncommon = [];
+    const dict = {};
     const s1Words = s1.split(' ');
     const s2Words = s2.split(' ');
-    const s1Counter = {};
-    const s2Counter = {};
     for (let word of s1Words) {
-        s1Counter[word] = (s1Counter[word] || 0) + 1;
+        dict[word] = (dict[word] || 0) + 1;
     }
     for (let word of s2Words) {
-        s2Counter[word] = (s2Counter[word] || 0) + 1;
+        dict[word] = (dict[word] || 0) + 1;
     }
-    
-    for (let word of s1Words) {
-        if (s1Counter[word] === 1 && !(word in s2Counter)) {
-            uncommon.push(word);
-        }
-    }
-    
-    for (let word of s2Words) {
-        if (s2Counter[word] === 1 && !(word in s1Counter)) {
+    for (let word in dict) {
+        if (dict[word] === 1) {
             uncommon.push(word);
         }
     }
