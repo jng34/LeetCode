75e397e60039a1727c677b42517f6459346063e6
James Ng <100321579+jng34@users.noreply.github.com>
2023-07-04 09:06:42 -0400

    Time: 67 ms (52.38%), Space: 42.7 MB (48.57%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.js
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.js
@@ -4,29 +4,23 @@
  */
 var countBattleships = function(board) {
     let count = 0;
-    let visited = new Set();
     for (let r=0; r<board.length; r++) {
         for (let c=0; c<board[0].length; c++) {
-            // helper fn to find battleship
-            if (findBattleships(r, c)) count++;
+            if (board[r][c] === '.') continue;
+            findBattleships(r, c);
+            count++;
         }
     }
     return count;
     
-    // Helper
+    
     function findBattleships(row, col) {
         const boundaryCheck = row < 0 || row >= board.length || col < 0 || col >= board[0].length;
-        if (boundaryCheck) return false;
-        if (board[row][col] === '.') return false;
-        
-        const point = row + ',' + col;
-        if (visited.has(point)) return false;
-        visited.add(point);
-        
+        if (boundaryCheck) return null;
+        if (board[row][col] === '.') return null;
+        board[row][col] = '.';
         findBattleships(row, col + 1);  //check vertically
         findBattleships(row + 1, col);  //check horizontally
-        
-        return true;
     }
 };
 
