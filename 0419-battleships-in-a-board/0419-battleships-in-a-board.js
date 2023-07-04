661b0befb000aecbdf1655c980b8493f4a096d13
James Ng <100321579+jng34@users.noreply.github.com>
2023-07-04 09:13:23 -0400

    Time: 66 ms (54.29%), Space: 42.5 MB (65.71%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.js
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.js
@@ -6,22 +6,14 @@ var countBattleships = function(board) {
     let count = 0;
     for (let r=0; r<board.length; r++) {
         for (let c=0; c<board[0].length; c++) {
-            if (board[r][c] === '.') continue;
-            findBattleships(r, c);
-            count++;
+            if (board[r][c] === 'X' 
+                && board[r][c-1] !== 'X' 
+                && (!board[r-1] || board[r-1][c] !== 'X')) {
+                count++;
+            }
         }
     }
     return count;
-    
-    
-    function findBattleships(row, col) {
-        const boundaryCheck = row < 0 || row >= board.length || col < 0 || col >= board[0].length;
-        if (boundaryCheck) return null;
-        if (board[row][col] === '.') return null;
-        board[row][col] = '.';
-        findBattleships(row, col + 1);  //check vertically
-        findBattleships(row + 1, col);  //check horizontally
-    }
 };
 
 
