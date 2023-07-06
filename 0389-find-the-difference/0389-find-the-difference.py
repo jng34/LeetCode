c6c8b09bd5ba60f7808e272f06585f6b69d5806e
James Ng <100321579+jng34@users.noreply.github.com>
2023-07-06 14:17:02 -0400

    Time: 51 ms (57.61%), Space: 16.4 MB (52.13%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,16 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        dict = {}
-        for letter in t:
-            if not letter in dict:
-                dict[letter] = 1
-            else:
-                dict[letter] += 1
-        
-        for letter in s:
-            dict[letter] -= 1
-        
-        for key in dict:
-            if dict[key] == 1:
-                return key
+        sortedS = sorted(s)
+        sortedT = sorted(t)
+        for i in range(len(sortedS)):
+            if sortedS[i] != sortedT[i]:
+                return sortedT[i]
+        return sortedT[-1]
         
