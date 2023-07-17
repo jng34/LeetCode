086a90f7a64f3f2935e0677815c2456f01680c48
James Ng <100321579+jng34@users.noreply.github.com>
2023-07-17 15:15:35 -0400

    Time: 70 ms (35.42%), Space: 44.2 MB (69.10%) - LeetHub

--- 1436-destination-city/1436-destination-city.js
+++ 1436-destination-city/1436-destination-city.js
@@ -3,24 +3,11 @@
  * @return {string}
  */
 var destCity = function(paths) {
-    let visited = {};
-    paths.forEach(path => {
-        const [start, end] = path;
-        if (visited[start]) {
-            visited[start] = [visited[start][0] + 1, 'none'];
-        } else {
-            visited[start] = [1, 'begin'];
-        }
-        
-        if (visited[end]) {
-            visited[end] = [visited[end][0] + 1, 'none'];
-        } else {
-            visited[end] = [1, 'dest'];
-        }
-    })
-    
-    for (let path in visited) {
-        if (visited[path][1] === 'dest') return path;
+    let visited = new Set();
+    for (let path of paths) {
+        visited.add(path[0]);
     }
-
+    for (let path of paths) {
+        if (!visited.has(path[1])) return path[1];
+    }
 };
