cf085ea83f4e80b0e815f2fd6003d70f1ca1ef90
James Ng <100321579+jng34@users.noreply.github.com>
2023-07-25 13:28:26 -0400

    Time: 64 ms (94.03%), Space: 51.3 MB (41.77%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -5,22 +5,20 @@
 var peakIndexInMountainArray = function(arr) {
     // Binary search
     // 2, 3, 7, 5, 3, 2, 1, 0
-    //       l  r
+    //    l     r
     //       m
-    
-    // 0, 1, 0
-    //    l  r
-    //    m
     // move pointers to right
     
     let l=0, r=arr.length-1;
     while (l<r) {
         const mid = Math.floor((l + r)/2);
-        if (arr[mid+1] > arr[mid]) {
-            l = mid+1;
+        if (arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]) {
+            return mid;
+        }
+        if (arr[mid-1] < arr[mid]) {
+            l = mid;
         } else {
             r = mid;
         }
     }
-    return l;
 };
