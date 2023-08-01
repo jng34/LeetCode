7a0ec606e3dd5d8ec3ef8f5110ebc70ca8b78840
James Ng <100321579+jng34@users.noreply.github.com>
2023-08-01 18:13:37 -0400

    Time: 58 ms (91.44%), Space: 43.5 MB (63.02%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -3,24 +3,11 @@
  * @return {number}
  */
 var maxNumberOfBalloons = function(text) {
-    const balloon = 'balon';
-    let freq = {};
-    let minOne = Infinity, minTwo = Infinity;
+    const freq = { 'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0 };
     for (let char of text) {
-        freq[char] = (freq[char] || 0) + 1;
+        freq[char]++;
     }
-    
-    for (let char of balloon) {
-        if (!freq[char]) return 0;
-        if (char === 'l' || char === 'o') {
-            minTwo = Math.min(minTwo, freq[char]);
-        } else {
-            minOne = Math.min(minOne, freq[char]);
-        }        
-    }
-    
-    if (minTwo < 2) return 0;
-    minTwo = Math.floor(minTwo/2);
-    
-    return minTwo <= minOne ? minTwo : minOne 
+    return Math.floor(
+        Math.min(freq['b'], freq['a'], freq['l']/2, freq['o']/2, freq['n'])
+    )
 };
