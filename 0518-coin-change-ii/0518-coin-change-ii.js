20288d79dabde9924945c9e842ed676e99845558
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-06 16:00:03 -0400

    Time: 2485 ms (5.03%), Space: 173.5 MB (5.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -3,25 +3,20 @@
  * @param {number[]} coins
  * @return {number}
  */
-var change = function(amount, coins) {
-    let memo = {};
-    return dp(amount, 0);
-    
-    function dp(val, i) {
-        // base cases return boolean
-        const key = val + ',' + i;
-        if (key in memo) return memo[key];
-        if (val === 0) return 1;
-        
-        const coin = coins[i];
-        let count = 0;
-        
-        for (let qty = 0; qty*coin <= val; qty++) {
-            const diff = val -(qty*coin);
-            count += dp(diff, i+1);
-        }
-        
-        memo[key] = count;
-        return memo[key];
+var change = function(amount, coins, i = 0, memo = {}) {
+    // base cases return boolean
+    const key = amount + ',' + i;
+    if (key in memo) return memo[key];
+    if (amount === 0) return 1;
+
+    const coin = coins[i];
+    let count = 0;
+
+    for (let qty = 0; qty * coin <= amount; qty++) {
+        const diff = amount - (qty * coin);
+        count += change(diff, coins, i + 1, memo);
     }
+
+    memo[key] = count;
+    return memo[key];
 }; 
