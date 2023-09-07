445d312104f0b25bd86f3616375866ec45590730
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-07 12:23:19 -0400

    Time: 93 ms (72.27%), Space: 45.9 MB (75.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -4,29 +4,18 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    let min = Infinity;
-    const memo = {};
-    const res = dp(amount, 0);
-    return res !== Infinity ? res: -1
     
-
-    function dp(val, i) {
-        // base cases
-        const key = val + ',' + i;
-        if (key in memo) return memo[key];
-        if (val === 0) return 0;
-        if (val < 0 || i === coins.length) return min;
-        
-        // subtract from current coin and stay on current coin
-        const path1 = 1 + dp(val-coins[i], i);
-        
-        // subtract from current coin and skip to next coin
-        const path2 = 1 + dp(val-coins[i], i+1);
-        
-        // skip to next coin
-        const path3 = dp(val, i+1);
-        
-        memo[key] = Math.min(min, path1, path2, path3);
-        return memo[key];
+    let dp = new Array(amount + 1).fill(Infinity);
+    // 0 num of coins to make 0
+    dp[0] = 0;
+    
+    for (let i = 1; i < dp.length; i++) {
+        for (let coin of coins) {
+            if (coin <= i) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
     }
+    
+    return dp[amount] !== Infinity ? dp[amount] : -1;
 };
