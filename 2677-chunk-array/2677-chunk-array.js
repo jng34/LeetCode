694d372af7e8e9a05a4402a19c57ce6cce2a6eb2
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-10 15:14:25 -0400

    Time: 52 ms (81.93%), Space: 45.1 MB (10.67%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,11 +4,18 @@
  * @return {Array[]}
  */
 var chunk = function(arr, size) {
-  let res = [];
-  let i = 0;
-  while (i < arr.length) {
-    res.push(arr.slice(i, i+size))
-    i = i + size;
+    let res = [];
+  let chunk = [];
+  let count = 0;
+  for (let i=0; i<arr.length; i++) {  
+    chunk.push(arr[i]);
+    count++;
+    if (count === size) {
+      res.push(chunk);
+      count = 0;
+      chunk = [];
+    }
   }
+  if (chunk.length) res.push(chunk);
   return res;
 };
