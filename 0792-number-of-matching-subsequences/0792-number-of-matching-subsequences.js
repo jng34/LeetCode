fb39e40af858042050f1c7a659325767fa12dfb6
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-22 13:35:27 -0400

    Time: 7562 ms (21.01%), Space: 51.4 MB (30.25%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+var numMatchingSubseq = function(s, words) {
+    let count = 0;
+    let seen = {};
+    for (let word of words) {
+        seen[word] = (seen[word] || 0) + 1;
+    }
+    for (let word in seen) {
+        const check = isSubsequence(word, s);
+        if (check) count += seen[word];
+    }
+    return count;
+};
+
+function isSubsequence(s, t) {
+    let n = 0;
+    for(let i=0; i < t.length; i++) {
+        if(s[n] === t[i]){
+          n++;
+        } 
+    } 
+    return n === s.length;
+}
