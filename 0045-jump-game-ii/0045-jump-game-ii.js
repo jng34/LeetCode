5edf1ce1bd59153971d7ac8a628491aeecef5782
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-22 18:17:45 -0400

    Time: 50 ms (89.18%), Space: 44.4 MB (26.95%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function jump(nums) {
+    let jumps = 0, curEnd = 0, curFarthest = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        curFarthest = Math.max(curFarthest, i + nums[i]);
+        if (curFarthest === nums.length -1) return ++jumps;
+        if (i == curEnd) {
+            jumps++;
+            curEnd = curFarthest;
+        }
+    }
+    return jumps;
+}
