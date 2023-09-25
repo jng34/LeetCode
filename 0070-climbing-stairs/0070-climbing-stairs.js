56475aacd82f3ad70abcdb116f3074be24e88ea7
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-24 22:25:18 -0400

    Time: 42 ms (93.39%), Space: 41.9 MB (35.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,68 +2,12 @@
  * @param {number} n
  * @return {number}
  */
-
-/*
-   initialize
-   ways = 0
-   use recursion
-*/
-
 var climbStairs = function(n) {
-    
-    //O(n) Time
-    //O(1) Space
-    
-    let one = 1;
-    let two = 1;
-    
-    for (let i = 0; i < n-1; i++) {
-        let curr = one;
-        one = one + two;
-        two = curr;
+    let dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i=3; i<n+1; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
     }
-        
-    return one;
-}
-
-
-// var climbStairs = function(n) {
-    
-//     // if n is 1, then return 1
-//     // if n is 2, then return 2
-//     // for n steps, we have to add the # of ways for the previous two steps
-//     // fibonacci seq
-//     // use tabulation 
-    
-//     //Time: O(n)
-//     //Space: O(n)
-    
-//     let table = new Array(n+1).fill(0);
-//     table[1] = 1;
-//     table[2] = 2;
-    
-//     for (let i = 3; i < table.length; i++) {
-//         table[i] = table[i-1] + table[i-2];
-//     }
-//     return table[n];
-// }
-
-
-
-
-
-
-// var climbStairs = function(n){
-//     let [one, two] = [1, 1];
-    
-//     for (let i = 0; i < n-1; i++) {
-//         let current = one;
-//         one = one + two;
-//         two = current;
-//     }
-//     return one;
-// };
-
-
-
-
+    return dp[n];
+};
