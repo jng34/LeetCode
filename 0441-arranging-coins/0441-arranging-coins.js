cb246318c001d2b29fd397f5c616577746c9bd14
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-28 11:53:09 -0400

    Time: 81 ms (23.78%), Space: 44 MB (49.81%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.js
+++ 0441-arranging-coins/0441-arranging-coins.js
@@ -4,14 +4,8 @@
  */
 var arrangeCoins = function(n) {
     let rows = 0;
-    let rem = n;
-    for (let i=0; i<n; i++) {
-        rem -= i+1;
-        if (rem < 0) {
-            break;
-        } else {
-            rows++;
-        }
-    }    
-    return rows;
+    while (rows <= n) {
+        n -= rows++;
+    }
+    return rows-1;
 };
