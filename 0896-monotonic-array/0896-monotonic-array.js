6e56604cc8e9814be0bc48ccb669959419228916
James Ng <100321579+jng34@users.noreply.github.com>
2023-09-28 23:43:34 -0400

    Time: 73 ms (77.14%), Space: 53.5 MB (30.34%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -6,9 +6,7 @@ var isMonotonic = function(nums) {
     // iterate to check for monotone increasing
     let increasing = true;
     for (let i=1; i<nums.length; i++) {
-        if (nums[i-1] <= nums[i]) {
-            continue;
-        } else {
+        if (nums[i-1] > nums[i]) {
             increasing = false;
             break;
         }
@@ -19,9 +17,7 @@ var isMonotonic = function(nums) {
     //iterate to check for monotone decreasing
     let decreasing = true;
     for (let j=1; j<nums.length; j++) {
-        if (nums[j-1] >= nums[j]) {
-            continue;
-        } else {
+        if (nums[j-1] < nums[j]) {
             decreasing = false;
             break;
         }
