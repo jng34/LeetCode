427ad39803e19463043f1a670159cf842c9f262b
James Ng <100321579+jng34@users.noreply.github.com>
2023-10-05 15:00:40 -0400

    Time: 63 ms (36.03%), Space: 43.3 MB (67.30%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,14 +3,33 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    const times = nums.length/3;
-    const freq = {};
-    nums.forEach(n => freq[n] = (freq[n] || 0) + 1);
-    
+    let num1, num2, count1 = 0, count2 = 0;
     let res = [];
-    for (let n in freq) {
-        if (freq[n] > times) res.push(n);
+    for (let n of nums) {
+        if (count1 === 0 && num2 !== n) {
+            num1 = n;
+            count1++;
+        } else if (count2 === 0 && num1 !== n) {
+            num2 = n;
+            count2++;
+        } else if (n === num1) {
+            count1++;
+        } else if (n === num2) {
+            count2++;
+        } else {
+            count1--;
+            count2--;
+        }
     }
+    
+    count1 = 0; count2 = 0;
+    for (let n of nums) {
+        if (num1 === n) count1++;
+        if (num2 === n) count2++;
+    }
+    
+    if (count1 > nums.length/3) res.push(num1);
+    if (count2 > nums.length/3) res.push(num2);
     
     return res;
 };
