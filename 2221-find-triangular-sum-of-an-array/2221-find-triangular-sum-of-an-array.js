c9008d1f7de72e4efa0b7d16143b9e603c6fbc25
James Ng <100321579+jng34@users.noreply.github.com>
2023-10-05 10:49:04 -0400

    Time: 82 ms (97.99%), Space: 43.8 MB (84.56%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -2,14 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var triangularSum = function(nums, tempArr=[]) {
-    // use recursion with base case n = 1
-    const n = nums.length;
-    if (n === 1) return nums[0];
+var triangularSum = function(nums) {
+    return helper(nums, nums.length);
     
-    for (let i=0; i<n-1; i++) {
-        tempArr[i] = (nums[i] + nums[i+1]) % 10; 
+    // use recursion with base case n = 1
+    function helper(arr, n) {
+        if (n === 1) return nums[0];
+        for (let i=0; i<n-1; i++) {
+            arr[i] = (nums[i] + nums[i+1]) % 10; 
+        }
+        return helper(arr, n-1);  
     }
     
-    return triangularSum(tempArr, []);
 };
