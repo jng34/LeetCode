d167c12ff4a2300b7ef43d238b931fd290a35c5d
James Ng <100321579+jng34@users.noreply.github.com>
2023-10-25 12:01:27 -0400

    Time: 66 ms (58.82%), Space: 47.3 MB (27.15%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.js
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.js
@@ -11,24 +11,44 @@
  * @return {number[]}
  */
 var largestValues = function(root) {
-  // Base case
-  if (!root) return [];
-  
-  // Breadth First Search - Queue
-  // Initialize queue with an array of the root node and row number 0
-  let queue = [[root, 0]];
-  let output = [];
-  
-  while (queue.length > 0) {
-    const [node, row] = queue.shift();
-    if (output[row] === undefined) {
-      output[row] = node.val;
-    } else {
-      output[row] = Math.max(node.val, output[row]);
+    /*
+        Create a hash map to store rows as keys and max values as values.
+        Keep track of each node value and row (0-indexed) in a queue
+        While queue.length > 0:
+            Destructure node, row in front of queue
+            If node.val in specific row > curr row's max val:
+                update max val
+            If node.right exists:
+                push into queue
+            If node.left exists:
+                push into queue
+        return Object.keys(hash map)
+    */
+    
+    if (!root) return [];
+    const maxVals = {};
+    let queue = [[root, 0]];
+    while (queue.length > 0) {
+        const [node, row] = queue.shift();
+        
+        if (maxVals[row]) {
+            maxVals[row] = Math.max(maxVals[row], node.val);
+        } else {
+            maxVals[row] = node.val;
+        }
+        
+        if (node.right) queue.push([node.right, row + 1]);
+        if (node.left) queue.push([node.left, row + 1]);
     }
-    if (node.left) queue.push([node.left, row+1])
-    if (node.right) queue.push([node.right, row+1])
-  }
-  
-  return output;
+    return Object.values(maxVals);
 };
+
+
+
+
+
+
+
+
+
+
