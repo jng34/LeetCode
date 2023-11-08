3e2b149eb4b0569e1486573d50921cf162c0d651
James Ng <100321579+jng34@users.noreply.github.com>
2023-11-08 09:36:46 -0500

    Time: 4076 ms (8.65%), Space: 52.6 MB (24.80%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var getWinner = function(arr, k) {
+    /*
+        [7,6,4,5,3,1,2]
+        if k >= length of arr, then return max element in arr
+        if k < length of arr, then simulate game.
+        let count = {} -> hashmap that keeps count of consecutive wins for each element.
+        Reverse arr.
+        Use a stack (loser) to use as temporary storage of loser to replace at end of line.
+        while count != k:
+            -pop off two elements (last two) to compare
+            -loser goes into the loser stack
+            -push winner back into arr
+            -increment winner's count
+            if winner's count = k, then return winner
+    */
+    
+    if (k >= arr.length) return Math.max(...arr);
+    
+    let winner = arr.shift();
+    let winCount = 0;
+    
+    while (arr.length > 0) {
+        const opponent = arr.shift();
+        if (winner > opponent) {
+            winCount++;
+            arr.push(opponent);
+        } else {
+            arr.push(winner);
+            winner = opponent;
+            winCount = 1;
+        }
+        if (winCount === k) return winner;
+    }
+};
