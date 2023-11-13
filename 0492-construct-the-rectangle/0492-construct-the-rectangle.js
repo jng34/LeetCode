de9e24321110ef83b484d1ea0cabe6ba3752d4dc
James Ng <100321579+jng34@users.noreply.github.com>
2023-11-13 08:39:47 -0500

    Time: 47 ms (90.61%), Space: 43.7 MB (5.52%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.js
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.js
@@ -14,12 +14,10 @@ var constructRectangle = function(area) {
            W^2 <= A
            L^2 >= A
     */
-    let l = 1;
     let w = Math.floor(Math.sqrt(area));
     while (w > 0) {
-        l = area/w;
-        if (l % 1 === 0) break;
+        if (area/w % 1 === 0) break;
         w--;               
     }
-    return [l, w];    
+    return [area/w, w];    
 };
