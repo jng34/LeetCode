4dc063cac3cbf743fe92f02ba406d00d75a20b47
James Ng <100321579+jng34@users.noreply.github.com>
2023-11-14 12:00:35 -0500

    Time: 70 ms (14.63%), Space: 43.6 MB (70.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,33 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-/*
-    -Create an object that counts # of each element
-    -Find elem with value greater than n/2
-    Time: O(n) 
-    Space: O(n)
-*/
 var majorityElement = function(nums) {
-    let majElem, count = 0;
+    /*
+        Get the frequency of each element in a hashmap
+        Find n/2.
+        Iterate through all elements (keys) and return 
+        the elem that has a freq > n/2.
+    */
     
-    for (let n of nums) {
-        if (count === 0) majElem = n;
-        count += (n === majElem ? 1 : -1)
+    const freq = {};
+    const n = nums.length;
+    
+    nums.forEach(n => {
+        freq[n] = (freq[n] || 0) + 1;
+    });
+    
+    for (let el in freq) {
+        if (freq[el] > n/2) return el;
     }
-    
-    return majElem;
 };
-
-// var majorityElement = function(nums) {
-//     const val = nums.length / 2;
-//     let counter = {};
-    
-//     for (let n of nums) {
-//         counter[n] = (counter[n] || 0) + 1;
-//     }
-    
-//     for (let n in counter) {
-//         if (counter[n] > val) return n;
-//     }
-// };
