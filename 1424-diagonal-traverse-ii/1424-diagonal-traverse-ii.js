82ce194785159b8a4d35dad48a13a8dbe348f68c
James Ng <100321579+jng34@users.noreply.github.com>
2023-11-25 09:54:58 -0500

    Time: 178 ms (90.94%), Space: 77.7 MB (90.94%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.js
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.js
@@ -2,57 +2,21 @@
  * @param {number[][]} nums
  * @return {number[]}
  */
-
-/*
-    [0,0] [0,1] [0,2] [0,3] [0,4]
-    
-    [1,0] [1,1] [1,2] [1,3] [1,4]
-    
-    [2,0] [2,1] [2,2] [2,3] [2,4]
-    
-    [3,0] [3,1] [3,2] [3,3] [3,4]
-    
-    [4,0] [4,1] [4,2] [4,3] [4,4]
-
-*/
-
-
 var findDiagonalOrder = function(nums) {
-    // Num of rows = nums.length
     const n = nums.length;
-    
-    // Create a hash object to store diagonal values.
-    const diagonals = {};
-    
-    // Create ans array.
+    let queue = [[0,0]];
     let ans = [];
-   
-    // Each diagonal has a sum.
-    // Iterate from bottom to top, left to right.
-    for (let r=n-1; r>=0; r--) {
-        for (let c=0; c<nums[r].length; c++) {
-            const sum = r+c;
-            if (diagonals[sum]) {
-                diagonals[sum].push(nums[r][c]);
-            } else {
-                diagonals[sum] = [nums[r][c]];
-            }
+    
+    while (queue.length > 0) {
+        const [r,c] = queue.shift();
+        ans.push(nums[r][c]);
+        if (c === 0 && r + 1 < n) {
+            queue.push([r + 1, c])
         }
-    }
-    
-    // Iterate through diagonals in order and fill ans.
-    let i=0;
-    
-    while (i in diagonals) {
-        ans.push(...diagonals[i]);
-        i++;
+        if (c + 1 < nums[r].length) {
+            queue.push([r, c + 1]);
+        }
     }
     
     return ans;
 };
-
-
-
-
-
-
