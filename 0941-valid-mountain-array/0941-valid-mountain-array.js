3d26b8175e4ac655bb6d69833debe34c89d01423
James Ng <100321579+jng34@users.noreply.github.com>
2023-11-26 10:30:23 -0500

    Time: 53 ms (80.92%), Space: 44.5 MB (57.39%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.js
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -5,37 +5,33 @@
 
 /*
   Two pointers, l and r.
-  Find max num and its index (first occurence), which is the peak.
-  Check indices left of peak for strictly increasing.
-  Check indices right of peak for strictly decreasing.
+  Two passes, starting from 1 and length of arr - 2.
+  Check indices from i = 1 for strictly increasing until 
+  we reach an index that doesn't fit condition.
+  Apply same check from the right i.e. length of arr - 2.
+  If l == r, then true, else false (return l === r).
 */
 
 var validMountainArray = function(arr) {
   const n = arr.length;
   
-  // Find max num in arr (peak).
-  const peak = Math.max(...arr);
+  let l = 1, r = n - 2;
   
-  // Find index of peak.
-  const peakIdx = arr.indexOf(peak);
-  
-  // Return false if peak index is at either end of arr.
-  if (peakIdx === 0 || peakIdx === n-1) return false;
-  
-  // Check indices left of peak for strictly increasing.
-  let l = 1;
-  while (l <= peakIdx) {
-    if (arr[l] <= arr[l-1]) return false;
+  while (l < n) {
+    if (arr[l-1] >= arr[l]) {
+      l--;
+      break;
+    }
     l++;
   }
   
-  // Check indices right of peak for strictly decreasing.
-  let r = n-2;
-  while (r >= peakIdx) {
-    if (arr[r] <= arr[r+1]) return false;
+  while (r >= 0) {
+    if (arr[r] <= arr[r+1]) {
+      r++;
+      break;
+    }
     r--;
   }
   
-  // Return true if left is increasing and right is decreasing
-  return true;
+  return l === r;
 };
