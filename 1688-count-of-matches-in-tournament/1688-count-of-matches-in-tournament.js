cffbbdcfb41cca94c08f2e77079d7d8409f65c47
James Ng <100321579+jng34@users.noreply.github.com>
2023-12-05 16:25:27 -0500

    Time: 53 ms (44.96%), Space: 41.7 MB (61.65%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    return n-1;
+  let matches = 0;  
+  
+  while (n > 1) {
+    // if n is even
+    if (n % 2 === 0) {
+      matches += n/2;
+      n = n/2;
+    } else {
+      // if n is odd
+      matches += (n-1)/2;
+      n = (n-1)/2 + 1;
+    }
+  }
+  
+  return matches;
 };
