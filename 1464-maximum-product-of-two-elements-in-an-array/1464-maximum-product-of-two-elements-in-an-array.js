47ce7bcb8399b5ae7207e5b5ebde4645b20564a2
James Ng <100321579+jng34@users.noreply.github.com>
2023-12-11 23:30:10 -0500

    Time: 51 ms (80.83%), Space: 41.8 MB (93.61%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -6,15 +6,21 @@
 // Find the two max values of nums
 // Use two variables max, secMax to store values
 // Iterate through nums to find max and secMax
-/*
-  e.g. [3,5,7,5]
-  max1 = 3
-  max2 = 7
-  
-*/
 // Return the product of their values minus 1
 
 var maxProduct = function(nums) {
-  nums.sort((a,b) => b-a);
-  return (nums[0]-1)*(nums[1]-1);
+  let max1 = -Infinity;
+  let max2 = -Infinity;
+  
+  // Find max and second max nums.
+  for (let i=0; i<nums.length; i++) {
+    if (nums[i] > max1) {
+      max2 = max1;
+      max1 = nums[i];
+    } else {
+      max2 = Math.max(max2, nums[i]);
+    }
+  }
+  
+  return (max1-1)*(max2-1);
 };
