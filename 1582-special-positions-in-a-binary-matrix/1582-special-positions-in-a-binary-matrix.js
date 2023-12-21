96861fff21a4772b29bb391bd592feea5d354839
James Ng <100321579+jng34@users.noreply.github.com>
2023-12-20 19:57:32 -0500

    Time: 64 ms (34.24%), Space: 44.8 MB (35.42%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.js
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.js
@@ -3,42 +3,35 @@
  * @return {number}
  */
 
-// Brute force
-// Iterate through matrix.
-// if matrix[r][c] === 1, 
-//   iterate through r to see if there are any other 1s
-//   iterate through c to see if thera re any other 1s
-//   if not, then increment count by 1
-// Return count 
+// Pre-compute ones using arrays for rowCount and colCount
+// Iterate through each square in matrix
+// If square = 1, 
+//   then check if rowCount = 1 and colCount = 1 
+//   if true, then increment count
+// Return count.
 
 var numSpecial = function(mat) {
   // Track special count.
   let count = 0;
   
-  // Iterate through matrix.
+  // Count all 1s in each row and column.
+  let rowCount = new Array(mat.length).fill(0);
+  let colCount = new Array(mat[0].length).fill(0);
+  
   for (let r=0; r<mat.length; r++) {
     for (let c=0; c<mat[0].length; c++) {
       if (mat[r][c] === 1) {
-        let good = true;
-        
-        // check rest of row for any other 1s.
-        for (let col=0; col<mat[0].length; col++) {
-          if (col !== c && mat[r][col] === 1) {
-            good = false;
-            break;
-          }
-        }  
-        
-        // check rest of col for any other 1s.
-        for (let row=0; row<mat.length; row++) {
-          if (row !== r && mat[row][c] === 1) {
-            good = false;
-            break;
-          }
-        }   
-      
-        // increment count if good is true.
-        if (good === true) count++;
+        rowCount[r]++;
+        colCount[c]++;
+      }
+    }
+  }
+  
+  // Iterate through matrix again.
+  for (let r=0; r<mat.length; r++) {
+    for (let c=0; c<mat[0].length; c++) {
+      if (mat[r][c] === 1 && rowCount[r] === 1 && colCount[c] === 1) {
+        count++;
       }
     }
   }
