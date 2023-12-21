a5d951aa08c1b4b17e31d85153f470d001b066ce
James Ng <100321579+jng34@users.noreply.github.com>
2023-12-21 15:09:58 -0500

    Time: 79 ms (5.39%), Space: 45.3 MB (7.65%) - LeetHub

--- 1436-destination-city/1436-destination-city.js
+++ 1436-destination-city/1436-destination-city.js
@@ -2,12 +2,20 @@
  * @param {string[][]} paths
  * @return {string}
  */
+
 var destCity = function(paths) {
-    let visited = new Set();
-    for (let path of paths) {
-        visited.add(path[0]);
-    }
-    for (let path of paths) {
-        if (!visited.has(path[1])) return path[1];
-    }
+  // Create an adjacency graph
+  const graph = {};
+  
+  for (let path of paths) {
+    const [start, end] = path;
+    if (!graph[start]) graph[start] = [];
+    if (!graph[end]) graph[end] = [];
+    graph[start].push(end);
+  }
+  
+  // Iterate through graph and return dest w empty array
+  for (let dest in graph) {
+    if (graph[dest].length === 0) return dest;
+  }
 };
