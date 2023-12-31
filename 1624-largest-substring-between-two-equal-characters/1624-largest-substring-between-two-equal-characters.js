f79156a0b8e5858015f3a293612dbc623649dc07
James Ng <100321579+jng34@users.noreply.github.com>
2023-12-31 17:52:37 -0500

    Time: 119 ms (6.45%), Space: 42.1 MB (74.19%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -5,25 +5,14 @@
 
 
 var maxLengthBetweenEqualCharacters = function(s) {
-    // Find first and last indices of each character.
     const freq = {};
-    for (let i=0; i<s.length; i++) {
-        if (freq[s[i]]) {
-            freq[s[i]][1] = i;
-        } else {
-            freq[s[i]] = [i];
-        }
-    }
-    
     let longest = -1;
     
-    // For each character with a start and end index, 
-    for (let ch in freq) {
-        if (freq[ch].length === 2) {
-            const [start, end] = freq[ch];
-            // update longest substring
-            // Find difference + (-1) to exclude the two characters
-            longest = Math.max(longest, end-start-1);
+    for (let i=0; i<s.length; i++) {
+        if (s[i] in freq) {
+            longest = Math.max(longest, i - freq[s[i]] - 1);
+        } else {
+            freq[s[i]] = i;
         }
     }
     
