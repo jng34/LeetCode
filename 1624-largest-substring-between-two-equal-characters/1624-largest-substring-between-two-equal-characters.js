095794ce3975e77d4d0aa24e6f27c9a8a0321f27
James Ng <100321579+jng34@users.noreply.github.com>
2023-12-31 17:41:10 -0500

    Time: 95 ms (11.83%), Space: 44.6 MB (13.98%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -4,32 +4,26 @@
  */
 
 
-
-
-
 var maxLengthBetweenEqualCharacters = function(s) {
-    // Find characters that appear more than once.
+    // Find first and last indices of each character.
     const freq = {};
     for (let i=0; i<s.length; i++) {
-        freq[s[i]] = (freq[s[i]] || 0) + 1;
+        if (freq[s[i]]) {
+            freq[s[i]][1] = i;
+        } else {
+            freq[s[i]] = [i];
+        }
     }
     
     let longest = -1;
     
-    // For each character, find the start and end indices
+    // For each character with a start and end index, 
     for (let ch in freq) {
-        if (freq[ch] > 1) {
-            let l = 0;
-            let r = s.length-1;
-            while (s[l] !== ch) {
-                l++;
-            }
-            while (s[r] !== ch) {
-                r--;
-            }
+        if (freq[ch].length === 2) {
+            const [start, end] = freq[ch];
             // update longest substring
             // Find difference + (-1) to exclude the two characters
-            longest = Math.max(longest, r-l-1);
+            longest = Math.max(longest, end-start-1);
         }
     }
     
