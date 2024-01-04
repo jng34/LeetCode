cf6f67825abab38e12fdc91f4125dba2100c32ac
James Ng <100321579+jng34@users.noreply.github.com>
2024-01-03 20:00:03 -0500

    Time: 61 ms (72.31%), Space: 41.9 MB (92.31%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
@@ -6,21 +6,19 @@
 var maxSumAfterPartitioning = function (arr, k) {
   // Dynamic programming: bottom-up approach
   const n = arr.length;
-  const dp = new Array(n).fill(0);
-  dp[0] = arr[0];
+  const dp = new Array(n+1).fill(0);
 
-  for (let i = 1; i < n; i++) {
-    let max = 0;
-    let maxVal = 0;
-
-    for (let j = i; j >= Math.max(0, i-k+1); j--) {
-      maxVal = Math.max(arr[j], maxVal);
-      if (j === 0) max = Math.max(max, maxVal*(i-j+1));
-      if (j > 0) max = Math.max(max, maxVal*(i-j+1) + dp[j-1]);
+  for (let i = 1; i < n + 1; i++) {
+    
+    let max = 0, maxVal = 0;
+    
+    for (let j = 1; j <= k && i - j >= 0; j++) {
+      maxVal = Math.max(arr[i - j], maxVal);
+      max = Math.max(max, maxVal*(j) + dp[i - j]);
     }
 
     dp[i] = max;
   }
 
-  return dp[n-1];
+  return dp[n];
 };
