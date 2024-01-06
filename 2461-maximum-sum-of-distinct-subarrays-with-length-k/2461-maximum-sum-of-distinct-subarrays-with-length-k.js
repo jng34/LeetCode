3516e180d6124202bd8f5d06dd8403fdb492cd60
James Ng <100321579+jng34@users.noreply.github.com>
2024-01-06 15:28:37 -0500

    Time: 118 ms (77.08%), Space: 80.9 MB (7.64%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+function maximumSubarraySum(nums, k) {
+  if (new Set(nums).size < k) return 0;
+
+  const n = nums.length;
+  let maxSum = 0, currSum = 0;
+  const freq = new Map();
+  
+  for (let i = 0; i < n; i++) {
+    const r = nums[i];
+    currSum += r;
+    freq.set(r, (freq.get(r) || 0) + 1);
+
+    if (i < k - 1) continue;
+    if (freq.size == k) maxSum = Math.max(maxSum, currSum);
+
+    const l = nums[i - k + 1];
+    currSum -= l;
+    freq.set(l, (freq.get(l) || 0) - 1);
+    if (freq.get(l) === 0) freq.delete(l);
+  }
+  return maxSum;
+};
