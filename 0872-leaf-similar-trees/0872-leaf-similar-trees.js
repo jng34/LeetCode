230231fe6cbaf7e7530296b77bddf330a2057dbf
James Ng <100321579+jng34@users.noreply.github.com>
2024-01-10 12:04:30 -0500

    Time: 56 ms (70.26%), Space: 44.8 MB (27.49%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.js
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.js
@@ -11,28 +11,35 @@
  * @param {TreeNode} root2
  * @return {boolean}
  */
+
+//DFS to find all leaf nodes
+//Save leaf nodes in an array
+//Compare the two arrays of both roots
+//Iterate through arrays, return true if same, false if not.
+
 var leafSimilar = function(root1, root2) {
-    // BFS
-    let leafVals1 = [];
-    let leafVals2 = [];
+  // Main code
+  const arr1 = findLeafNodes(root1); 
+  const arr2 = findLeafNodes(root2);
+  if (arr1.length !== arr2.length) return false;
+  for (let i=0; i<arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+  
+  
+  // Helper function - DFS 
+  function findLeafNodes(root) {
+    let arr = [];
+    let stack = [root];
     
-    function findLeaves(node, vals) {
-        if (!node.left && !node.right) {
-            vals.push(node.val);
-            return;
-        }    
-        if (node.left) findLeaves(node.left, vals);
-        if (node.right) findLeaves(node.right, vals);
+    while (stack.length > 0) {
+      const curr = stack.pop();
+      if (!curr.right && !curr.left) arr.push(curr.val);
+      if (curr.right) stack.push(curr.right);
+      if (curr.left) stack.push(curr.left);
     }
     
-    findLeaves(root1, leafVals1);
-    findLeaves(root2, leafVals2);
-    
-    while (leafVals1.length || leafVals2.length) {
-        const curr1 = leafVals1.pop();
-        const curr2 = leafVals2.pop();
-        if (curr1 !== curr2) return false;
-    }
-    
-    return true;
+    return arr;
+  }
 };
