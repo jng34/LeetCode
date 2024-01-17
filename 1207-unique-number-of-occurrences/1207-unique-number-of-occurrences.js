e53fb711d1c997b51f5590ca8503bdeb7177c6bc
James Ng <100321579+jng34@users.noreply.github.com>
2024-01-16 23:18:16 -0500

    Time: 60 ms (32.99%), Space: 42.9 MB (39.30%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -6,14 +6,7 @@ var uniqueOccurrences = function(arr) {
   // Find freq of each value
   const freq = {};
   arr.forEach(n => freq[n] = (freq[n] || 0) + 1);
-  
-  // Iterate through each freq and add to a set
-  const uniqueFreqs = new Set();
-  for (let n in freq) {
-    // return false if set already contains freq
-    if (uniqueFreqs.has(freq[n])) return false;
-    uniqueFreqs.add(freq[n]);
-  }
 
-  return true;
+  // Return true if num of freqs equals num of values, else false
+  return new Set([...Object.values(freq)]).size === Object.keys(freq).length;
 };
