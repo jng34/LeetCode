56e0857e4f1c8c58501ed2a8fc484196877f3205
James Ng <100321579+jng34@users.noreply.github.com>
2024-01-19 11:19:35 -0500

    Time: 52 ms (53.14%), Space: 48.7 MB (7.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,12 +2,15 @@
  * @param {number} n
  * @return {number}
  */
+
+// Fibonacci sequence: 1,1,2,3,5,8
 var climbStairs = function(n) {
-    let dp = new Array(n+1).fill(0);
-    dp[1] = 1;
-    dp[2] = 2;
-    for (let i=3; i<n+1; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
-    }
-    return dp[n];
+  const dp = new Array(n+1).fill(0);
+  dp[1] = 1; // One way to get to step n = 1
+  dp[2] = 2; // Two ways to get to step n = 2
+  for (let i = 3; i < n+1; i++) {
+    // Each step is the sum of previous two ways
+    dp[i] = dp[i-1] + dp[i - 2];
+  }
+  return dp[n];
 };
