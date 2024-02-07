b4406aa33fc5669da07aeada1b39842258b8f0ad
James Ng <100321579+jng34@users.noreply.github.com>
2024-02-07 17:47:01 -0500

    Time: 128 ms (14.58%), Space: 62.2 MB (6.17%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function (allowed, words) {
+  // Main code
+  // create set for allowed
+  const allowedSet = new Set(allowed);
+  let ans = 0;
+  for (let word of words) {
+    if (checkConsistency(word)) ans++;
+  }
+  return ans;
+
+
+  // helper function to check if all chars in word appears in allowed
+  function checkConsistency(word) {
+    const wordSet = new Set(word);
+    return [...wordSet].every((ch) => allowedSet.has(ch));
+  }
+};
