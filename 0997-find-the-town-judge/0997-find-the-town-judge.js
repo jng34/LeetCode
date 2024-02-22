3d755fabf09dbd4e84c67ecf40439ab88cc311f8
James Ng <100321579+jng34@users.noreply.github.com>
2024-02-22 17:18:03 -0500

    Time: 88 ms (60.76%), Space: 58.5 MB (15.44%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -5,40 +5,33 @@
  */
 var findJudge = function (n, trust) {
   if (n === 1) return 1;
-  // keep track of trusters
-  const trusters = {};
-  // keep track of trustees
+
+  const trusters = new Set();
+  const judgeCandidates = [];
+
+  // keep track of number of people who trust the trustees
   const trustees = {};
 
   for (let trustPair of trust) {
     const [a, b] = trustPair; // a = truster, b = trustee
+
+    // add all trusters
+    trusters.add(a);
     
-    if (a in trusters) {
-      trusters[a].push(b);
-    } else {
-      trusters[a] = [b];
-    }
-
-    if (b in trustees) {
-      trustees[b].push(a);
-    } else {
-      trustees[b] = [a];
-    }
+    // count number of people who trust the trustee
+    trustees[b] = (trustees[b] || 0) + 1;
   }
 
-  const judges = [];
-  // find judge, who is not in trusters obj
+  // Criteria 1: find all persons who do not trust others
   for (let i = 1; i <= n; i++) {
-    if (!(i in trusters)) {
-      judges.push(i);
-    }
+    if (!trusters.has(i)) judgeCandidates.push(i);
   }
 
-  // check if every person trusts the judge by checking if 
-  for (let i = 0; i < judges.length; i++) {
-    const judgeCandidate = judges[i];
-    if (trustees[judgeCandidate]?.length === n - 1) return judgeCandidate;
+  // Criteria 2: find the possible judge who everyone else trusts
+  for (let i = 0; i < judgeCandidates.length; i++) {
+    const judge = judgeCandidates[i];
+    if (trustees[judge] === n - 1) return judge;
   }
 
-  return -1; 
+  return -1;
 };
