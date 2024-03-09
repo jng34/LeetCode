4732479e6ab2c4268fff042fbbbc86a71bcd26f7
James Ng <100321579+jng34@users.noreply.github.com>
2024-03-08 22:58:12 -0500

    Time: 58 ms (94.39%), Space: 56.3 MB (86.99%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -5,14 +5,20 @@
  */
 var getCommon = function(nums1, nums2) {
   // iterate through both nums1 and nums2
-  // use a set to track 'seen' numbers in nums1
-  // if set has number, return num
+  // use two pointers to track elements
+  // increment p1 if nums1 elem < nums2 elem, 
+  // else increment p2
   
-  const seen = new Set();  
-  nums1.forEach(n => seen.add(n));
+  let p1 = 0, p2 = 0;
+  const n = nums1.length, m = nums2.length;
   
-  for (let n of nums2) {
-    if (seen.has(n)) return n;
+  while (p1 < n && p2 < m) {
+    if (nums1[p1] === nums2[p2]) return nums1[p1];
+    if (nums1[p1] < nums2[p2]) {
+      p1++;
+    } else {
+      p2++;
+    }
   }
   
   return -1;
