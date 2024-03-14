c08bb370000b66ec8ec22149ee7ddf1c353861c4
James Ng <100321579+jng34@users.noreply.github.com>
2024-03-14 18:47:54 -0400

    Time: 54 ms (74.15%), Space: 50.8 MB (72.22%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    // return String[]
+    let ans = [];
+    findRootPath(root, `${root.val}`);
+    return ans;
+
+    // helper function DFS
+    function findRootPath(node, path) {
+      if (!node || (!node.left && !node.right)) return ans.push(path); 
+      if (node.left) findRootPath(node.left, path + '->' + node.left.val);
+      if (node.right) findRootPath(node.right, path + '->' + node.right.val);
+      return path;
+    }
+};
