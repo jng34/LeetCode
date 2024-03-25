bb2ab9f3d71d04b96c519cb44234ca20720485ab
James Ng <100321579+jng34@users.noreply.github.com>
2024-03-25 11:19:55 -0400

    Time: 63 ms (81.08%), Space: 53.3 MB (57.17%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -10,20 +10,24 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-
-/*
-   1   ->   2 
- slow      
- fast
- if slow == fast
-*/
 var detectCycle = function(head) {
-  let seen = new Set();
-  let curr = head;
-  while (curr) {
-    if (seen.has(curr)) return curr;
-    seen.add(curr);
-    curr = curr.next;
+  let slow = head, fast = head;
+  
+  while (fast && fast.next && fast.next.next) {
+    // find intersection point of cycle
+    slow = slow.next;
+    fast = fast.next.next;
+    
+    if (slow === fast) {
+      // find starting point of cycle
+      slow = head;
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      return fast;
+    }
   }
+  
   return null;
 };
