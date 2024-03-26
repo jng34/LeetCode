1a4988ac9f7daa260b707a95f6bf19f63394e98b
James Ng <100321579+jng34@users.noreply.github.com>
2024-03-26 14:01:05 -0400

    Time: 70 ms (95.24%), Space: 57.8 MB (73.49%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -2,14 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDuplicates = function(nums) {
-  const seen = new Set();
+var findDuplicates = function (nums) {
   let ans = [];
-  
-  for (let n of nums) {
-    if (seen.has(n)) ans.push(n);
-    seen.add(n);
+  const n = nums.length;
+
+  // use the absolute value of the nums (minus 1) as an idx
+  // if nums[m - 1] is negative, then m is a duplicate
+  // modify nums[m - 1] to be negative.
+  for (let i = 0; i < n; i++) {
+    const m = Math.abs(nums[i]);
+    if (nums[m - 1] < 0) {
+      ans.push(m);
+    }
+    nums[m - 1] *= -1;
   }
-  
+
   return ans;
 };
