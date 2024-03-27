95750483dd52cd8e5f2b61e15ed480b6de07a8a9
James Ng <100321579+jng34@users.noreply.github.com>
2024-03-26 21:51:59 -0400

    Time: 534 ms (75.46%), Space: 49.3 MB (27.79%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -3,13 +3,16 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-  // Linear time O(n)
-  // iterate from 1 up to num, and find all divisors
+  // all odd numbers are false
+  if (num % 2 === 1) return false;
+  
+  // Linear time O(n/2), linear space O(n)
+  // iterate from 1 up to num / 2, and find all divisors
   // track sum of divisors and return sum === num
   
   let sum = 0;
   
-  for (let i = 1; i < num; i++) {
+  for (let i = 1; i <= num/2; i++) {
     if (num % i === 0) sum += i;
   }
   
