7124080f445353736907d5c74d2b43de3f3fb26a
James Ng <100321579+jng34@users.noreply.github.com>
2024-05-22 18:27:10 -0400

    Time: 1107 ms (54.30%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -2,7 +2,7 @@
 SELECT 
   SUBSTRING(trans_date, 1, 7) as month,
   country, 
-  COUNT(id) as trans_count,
+  COUNT(*) as trans_count,
   SUM(IF(state = "approved", 1, 0)) as approved_count, 
   SUM(amount) as trans_total_amount, 
   SUM(IF(state = "approved", amount, 0)) as approved_total_amount
