c2ff9977d80e731c2988987f73b04731d870c5ca
James Ng <100321579+jng34@users.noreply.github.com>
2024-06-12 13:43:59 -0400

    Time: 65 ms (21.97%), Space: 49.6 MB (56.05%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -4,27 +4,23 @@
  * @return {number[]}
  */
 var relativeSortArray = function(arr1, arr2) {
-    const freq = {};
-    arr1.forEach((n) => freq[n] = (freq[n] || 0) + 1);
-    
     let ans = [];
     for (let i = 0; i < arr2.length; i++) {
         const n = arr2[i];
-        for (let j = freq[n]; j > 0; j--) {
-            ans.push(n);
-            freq[n]--;
-        }
-    }
-    
-    let temp = [];
-    for (let n in freq) {
-        if(freq[n]!==0){
-            for (let j = freq[n]; j > 0; j--) {
-                temp.push(n);                
+        for (let j = 0; j < arr1.length; j++) {
+            const m = arr1[j];
+            if (m === n) {
+                ans.push(m);
+                arr1[j] = -1;
             }
         }
     }
-    temp.sort((a,b) => a-b); // ASC
     
-    return [...ans, ...temp];
+    arr1.sort((a,b) => a-b);
+    
+    for (let n of arr1) {
+        if (n !== -1) ans.push(n);
+    }
+    
+    return ans;
 };
