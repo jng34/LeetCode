5b6d0e1dae4993561feba5581fdf14ab59db89c8
James Ng <100321579+jng34@users.noreply.github.com>
2024-07-02 16:07:16 -0400

    Time: 59 ms (18.88%), Space: 49.3 MB (11.01%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.js
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -7,8 +7,8 @@ var threeConsecutiveOdds = function(arr) {
     const first = arr[i-2]
     const second = arr[i-1];
     const third = arr[i];
-    if (first % 2 === 1 && second % 2 === 1 & third % 2 === 1) {
-      return true;
+    if (first % 2 === 1 && second % 2 === 1) {
+      if (third % 2 === 1) return true;
     }
   }
   return false;
