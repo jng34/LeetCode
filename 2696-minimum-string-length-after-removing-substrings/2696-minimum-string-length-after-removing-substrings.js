4aa786d48ba4684ae1a982dbf0f73e774a59721e
James Ng <100321579+jng34@users.noreply.github.com>
2024-10-07 18:56:07 -0400

    Time: 106 ms (8.39%), Space: 54.2 MB (27.97%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
@@ -3,26 +3,21 @@
  * @return {number}
  */
 var minLength = function(s) {
-  return checkAB(s);
-
-  function checkAB(str) {
-    let ab = str.indexOf('AB');
-    while (ab >= 0) {
-      str = str.substring(0, ab) + str.substring(ab + 2);
-      ab = str.indexOf('AB');
+  let stack = [];
+  for (let ch of s) {
+    if (!stack.length) {
+      stack.push(ch);
+      continue;
     }
-    const cd = str.indexOf('CD');
-    return (cd >= 0) ? checkCD(str) : str.length;
+    
+    if (ch === 'B' && stack[stack.length - 1] === 'A') {
+      stack.pop();
+    } else if (ch === 'D' && stack[stack.length - 1] === 'C') {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
   }
   
-  function checkCD(str) {
-    let cd = str.indexOf('CD');
-    while (cd >= 0) {
-      str = str.substring(0, cd) + str.substring(cd + 2);
-      cd = str.indexOf('CD');
-    }
-    const ab = str.indexOf('AB');
-    return (ab >= 0) ? checkAB(str) : str.length;
-  }
-
+  return stack.length;
 };
