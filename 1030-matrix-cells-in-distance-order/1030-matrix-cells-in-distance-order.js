6c8750e10a335e1b16a7025b6b63f6ecf682e944
James Ng <100321579+jng34@users.noreply.github.com>
2024-11-29 12:45:39 -0500

    Time: 12 ms (86.05%), Space: 60 MB (72.73%) - LeetHub

---
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} rows
+ * @param {number} cols
+ * @param {number} rCenter
+ * @param {number} cCenter
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(rows, cols, rCenter, cCenter) {
+    // Create corresponding coordinates array
+    const coordinates = [];
+    
+    // For each element, find distance of element from center
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            coordinates.push([r, c]);
+        }    
+    }
+    
+    // Sort array of coordinates array based off of distances array
+    return coordinates.sort((a, b) => {
+        const [r1, c1] = a;
+        const [r2, c2] = b;
+        
+        // compare distances of each coordinate pair
+        const d1 = Math.abs(r1 - rCenter) + Math.abs(c1 - cCenter);
+        const d2 = Math.abs(r2 - rCenter) + Math.abs(c2 - cCenter);
+        
+        return d1 - d2;
+    })
+    
+};
