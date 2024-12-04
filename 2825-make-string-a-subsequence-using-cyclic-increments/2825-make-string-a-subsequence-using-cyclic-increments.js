9c096d31ae2e23136231c728fc5047e5c5c5a326
James Ng <100321579+jng34@users.noreply.github.com>
2024-12-04 12:55:34 -0500

    Time: 9 ms (93.75%), Space: 55.5 MB (86.36%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {boolean}
+ */
+var canMakeSubsequence = function(str1, str2) {
+    // Two pointers: i tracks str1, j tracks str2
+    let i = 0, j = 0;
+    
+    // str2 = 'ad'
+                 // j
+    // str1 = 'abc'
+                  // i
+    while (j < str2.length) {
+        const asciiCode2 = str2[j].charCodeAt(0);
+        while (i < str1.length) {
+            const asciiCode1 = str1.charCodeAt(i);
+            const nextCode1 = (asciiCode1 === 122 ? 97 : asciiCode1 + 1);
+            if (asciiCode2 === asciiCode1 || asciiCode2 === nextCode1) {
+                i++;
+                j++;
+                break;
+            } else {
+                i++;
+            }
+        }
+        if (i === str1.length) {
+            if (j === str2.length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+            
+    }
+    return true;
+};
